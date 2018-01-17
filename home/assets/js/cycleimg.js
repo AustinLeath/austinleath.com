68f3aea47814887ace8810dce8fb78004e29a4c7
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-01-17 09:23:49 -0600

    Current version as of 1/17/18

--- home/assets/js/cycleimg.js
+++ home/assets/js/cycleimg.js
@@ -1,11 +1,11 @@
 var img = $('img[id^="image-"]').hide(),
-		i = 0;
+  i = 0;
 
 (function cycle() {
 
-		img.eq(i).fadeIn(800)
-							.delay(5000)
-							.fadeOut(800, cycle);
+			 img.eq(i).fadeIn(800)
+							  .delay(5000)
+			  				.fadeOut(800, cycle);
 
 		i = ++i % img.length;
 
--- home/assets/js/cycletxt.js
+++ home/assets/js/cycletxt.js
@@ -1,11 +1,11 @@
 var h4 = $('h4[id^="cycletxt-"]').hide(),
-		i = 0;
+  i = 0;
 
-(function cycle() {
+(function cyclea() {
 
-		h4.eq(i).fadeIn(600)
-							.delay(4000)
-							.fadeOut(600, cycle);
+			 h4.eq(i).fadeIn(800)
+							  .delay(5000)
+			  				.fadeOut(800, cyclea);
 
 		i = ++i % h4.length;
 
