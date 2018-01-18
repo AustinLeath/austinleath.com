562f3858510934adf522de6e1165634abdfb8a76
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-01-18 10:31:39 -0600

    Current version as of 1/18/18

--- home/assets/js/cycletxt.js
+++ home/assets/js/cycletxt.js
@@ -1,12 +1,12 @@
-var h4 = $('h4[id^="cycletxt-"]').hide(),
+var p = $('p[id^="cycletxt-"]').hide(),
   i = 0;
 
 (function cyclea() {
 
-			 h4.eq(i).fadeIn(800)
+			 p.eq(i).fadeIn(800)
 							  .delay(5000)
 			  				.fadeOut(800, cyclea);
 
-		i = ++i % h4.length;
+		i = ++i % p.length;
 
 })();
