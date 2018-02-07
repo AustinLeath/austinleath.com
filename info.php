061bd0a70ee16a2daa8d40b323373aaf9c259da2
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-07 12:58:59 -0600

    Current version as of 2/7/18

--- info.php
+++ info.php
@@ -1,3 +1,19 @@
-<?php
-	phpInfo();
-?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Austin Leath | PHP Info</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="keywords" content="HTML,CSS,XML,JavaScript,Web,Engineer,Resume,CSS3,HTML5,Austin,Leath,Lethal,Game,Studios,LethalGameStudios">
+		<meta name="description" content="This page is the PHP engine info page">
+		<meta name="author" content="Austin Leath">
+		<meta name='copyright' content='Lethal Game Studios'>
+		<meta name='owner' content='Austin Leath'>
+		<link rel="shortcut icon" href="images/main/favicon.ico" />
+	</head>
+	<body>
+		<?php
+			phpInfo();
+		?>
+	</body>
+</html>
