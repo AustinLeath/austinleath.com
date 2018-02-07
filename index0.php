bc344b4de42cb52876554140e18832daaa9a0a70
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-07 16:55:37 -0600

    Current

--- index0.php
+++ index0.php
@@ -75,7 +75,7 @@
 
 
 
-										<h5><?php $today = date("F j, Y, "); echo "Current Time: <h1 id="timer"></h1>"; . $time ?></h5>
+										<h5><?php $today = date("F j, Y, "); echo "Current Time: <h1 id="timer"></h1>"; . $today ?></h5>
 
 
 										<h1><?php $today = date("F j, Y, g:i a"); echo "Current Time: " . $today; ?></h1>
