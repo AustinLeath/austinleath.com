227e2aa4eb8d2d11ff07702734905761b20f1829
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-07 13:07:44 -0600

    Current

--- info.php
+++ info.php
@@ -11,58 +11,11 @@
 		<meta name='owner' content='Austin Leath'>
 		<link rel="shortcut icon" href="images/main/favicon.ico" />
 	</head>
-	<header id="header">
-		<h1 id="logo"><a href="">Austin Leath | Resume</a></h1>
-		<nav id="nav">
-			<ul>
-				<li>
-					<a href="">Other Information</a>
-						<ul>
-						 <li><a href="git/github.html">GitHub Code</a></li>
-						 <li><a href="tools.html">Development Tools</a></li>
-						 <li><a href="rcs/index.html">Rileigh Sayen</a></li>
-						 <li><a href="info/aboutdeveloper.html">About Me</a></li>
-						 <li><a href="vrsenal/index.html">VRsenal</a></li>
-						 <li><a href="#">Return Home</a></li>
-						</ul>
-				<!--DIVIDER-->
-							 <li>
-								<a href="">Legal</a>
-								 <ul>
-									 <li><a href="legal/apachelicense.html">Apache License</a></li>
-									 <li><a href="legal/mitlicense.html">MIT License</a></li>
-								 </ul>
-								</li>
-				 <!--DIVIDER-->
-						 <li>
-							 <a href="">Streaming</a>
-								 <ul>
-									<li><a href="streaming/gamesweplay.html">Games We Play</a></li>
-									<li><a href="streaming/streamerswewatch.html">Streamers We Watch</a></li>
-									<li><a href="streaming/streaming.html">Streaming</a></li>
-									<li><a href="streaming/form.html">Form</a></li>
-								 </ul>
-								</li>
-				 <!--DIVIDER-->
-							 </li>
-							</ul>
-						 </li>
-				 <!-- Add Another Button Here -->
-			 </ul>
-		 </nav>
-	 </header>
 	<body>
 		<?php
 			phpInfo();
+			echo '<a href="index.html">Return Home</a>';
 		?>
 	</body>
 	<!--Scripts-->
-	<script src="home/assets/js/jquery.min.js"></script>
-	<script src="home/assets/js/jquery.scrolly.min.js"></script>
-	<script src="home/assets/js/jquery.dropotron.min.js"></script>
-	<script src="home/assets/js/jquery.scrollex.min.js"></script>
-	<script src="home/assets/js/skel.min.js"></script>
-	<script src="home/assets/js/util.js"></script>
-	<!--[if lte IE 8]><script src="home/assets/js/ie/respond.min.js"></script><![endif]-->
-	<script src="home/assets/js/main.js"></script>
 </html>
