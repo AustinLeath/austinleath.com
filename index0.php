0ffcfbc63f077e02c3509501be866f55ba23d150
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-07 11:17:42 -0600

    Current version as of 2/7/18

--- index0.php
+++ index0.php
@@ -69,41 +69,49 @@
 							<h5 id="cycletxt-3">Use the menu at the top of the page to see my entire site</h5>
 						</header>
 										<!-- Content -->
-													<section id="content">
-														<a class="image fit"><img src="images/MTN.jpg" alt="" /></a>
-														<h3>Defining Traits</h3>
-														<?php echo "hello world local script";?>
 
-														<?php include 'main.php';?>
-														<p>Austin is a 17 year old high school student who primarily focuses on Web Design and Graphic Design but also enjoys Game development on the side.</p>
-													<!-- Skills -->
-														<ul>
-														 <strong>General Knowledge</strong>
-														 <li>Computer System Building - 5 years</li>
-														 <li>GIT / Version Control - 4 years</li>
-														 <li>SSH - 2 years</li>
-														</ul>
-															 <!--Divider-->
-																	<ul>
-																		<strong>Adobe Products</strong>
-																		<li>Adobe Illustrator - 1 year</li>
-																		<li>Adobe Photoshop - 2 years</li>
-																		<li>Adobe Premiere Pro - 1 year</li>
-																	</ul>
-																<!--Divider-->
-																<ul>
-																 <strong>Web Languages</strong>
-																	<li>HTML / HTML5 - 4 years</li>
-																	<li>CSS / CSS3 - 4 years</li>
-																	<li>Javascript - 2 years</li>
-																</ul>
-															<!--Divider-->
-															<ul>
-															 <strong>Programming Languages</strong>
-																<li>C# - 1 year</li>
-												       </ul>
-										      </ul>
-								</section>
+										<?php
+       $ip='94.219.40.96';
+       print_r(geoCheckIP($ip));
+       //Array ( [domain] => dslb-094-219-040-096.pools.arcor-ip.net [country] => DE - Germany [state] => Hessen [town] => Erzhausen )
+
+       //Get an array with geoip-infodata
+       function geoCheckIP($ip)
+       {
+               //check, if the provided ip is valid
+               if(!filter_var($ip, FILTER_VALIDATE_IP))
+               {
+                       throw new InvalidArgumentException("IP is not valid");
+               }
+
+               //contact ip-server
+               $response=@file_get_contents('http://www.netip.de/search?query='.$ip);
+               if (empty($response))
+               {
+                       throw new InvalidArgumentException("Error contacting Geo-IP-Server");
+               }
+
+               //Array containing all regex-patterns necessary to extract ip-geoinfo from page
+               $patterns=array();
+               $patterns["domain"] = '#Domain: (.*?)&nbsp;#i';
+               $patterns["country"] = '#Country: (.*?)&nbsp;#i';
+               $patterns["state"] = '#State/Region: (.*?)<br#i';
+               $patterns["town"] = '#City: (.*?)<br#i';
+
+               //Array where results will be stored
+               $ipInfo=array();
+
+               //check response from ipserver for above patterns
+               foreach ($patterns as $key => $pattern)
+               {
+                       //store the result in array
+                       $ipInfo[$key] = preg_match($pattern,$response,$value) && !empty($value[1]) ? $value[1] : 'not found';
+               }
+
+               return $ipInfo;
+       }
+
+?>
 					</div>
 				</div>
 			<!-- Footer -->
