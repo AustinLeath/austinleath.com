3b4f14a9bf88666c5060e9dc487f4fa84f255017
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-07 14:36:13 -0600

    Current version as of 2/7/18

--- index0.php
+++ index0.php
@@ -67,7 +67,7 @@
 							<h5 id="cycletxt-1">The following page contains a compiled list of my skills and experiences</h5>
 							<h5 id="cycletxt-2">Welcome to my site</h5>
 							<h5 id="cycletxt-3">Use the menu at the top of the page to see my entire site</h5>
-							<h5 id="cycletxt-4"></h5>
+							<h5 id="cycletxt-4"><?php $a = 2; $b = 2; echo ($a + $b); ?></h5>
 						</header>
 										<!-- Content -->
 							<?php
