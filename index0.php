0c47953b014804ebbb5854f02a2fa09b906a27cb
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-08 09:08:21 -0600

    Current

--- index0.php
+++ index0.php
@@ -69,7 +69,7 @@
 							<h5 id="cycletxt-2">Welcome to my site</h5>
 							<h5 id="cycletxt-3">Use the menu at the top of the page to see my entire site</h5>						</header>
 										<!-- Content -->
-						<li><?php $today = date("F j, Y,"); echo "Current Time: " . $today; ?><h1 id="timer"></h1></li>
+						<h1><?php $today = date("F j, Y,"); echo "Current Time: " . $today; echo "<h1 id="timer"></h1>"; ?>
 					</div>
 				</div>
 			<!-- Footer -->
