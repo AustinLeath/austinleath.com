3f1c36416596655a20f255ea09f007005ec84810
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-12 14:38:47 -0600

    Current version as of 2/12/18

--- home/assets/php/action.php
+++ home/assets/php/action.php
@@ -3,6 +3,8 @@
 
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
+Your category is: <?php echo $_GET["category"]; ?>
+Your message to the server is: <?php echo $_GET["message"]; ?>
 
 </body>
 </html>
