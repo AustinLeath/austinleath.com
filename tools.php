9dd75bdb5f1b2d23d948c2869b8ef57136597569
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-12 10:05:48 -0600

    Current version as of 2/12/18

--- tools.php
+++ tools.php
@@ -33,7 +33,7 @@
 									 <li><a href="rcs/index.php">Rileigh Sayen</a></li>
 									 <li><a href="info/aboutdeveloper.html">About Me</a></li>
 									 <li><a href="vrsenal/index.php">VRsenal</a></li>
-									 <li><a href="ndex.html">Return Home</a></li>
+									 <li><a href="index.html">Return Home</a></li>
 									 <li><button class="button small" type="button" onclick="myFunction()">Change the style</button></li>
 									 <li><button class="button small" type="button" onclick="myFunction1()">Change the style back</button></li>
 								  </ul>
