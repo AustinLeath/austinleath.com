c6575ae6d7f4a398fb7d35b79ad1bb29a95c1234
Austin Leath <gameking2783@gmail.com>
2018-02-12 22:45:45 -0600

    Current

--- home/assets/php/action.php
+++ home/assets/php/action.php
@@ -105,7 +105,7 @@
 			<script src="../assets/js/jquery.scrollex.min.js"></script>
 			<script src="../assets/js/skel.min.js"></script>
 			<script src="../assets/js/util.js"></script>
-			<script src="../assets/js/cycletxt.js"></script>
+			<script src="./assets/js/cycletxt.js"></script>
 			<!--[if lte IE 8]><script src="home/assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="../assets/js/main.js"></script>
 
