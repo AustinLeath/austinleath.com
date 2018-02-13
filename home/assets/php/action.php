5d35ed5279032791d8014be34424d3caac01024a
Austin Leath <gameking2783@gmail.com>
2018-02-12 22:32:26 -0600

    Current version as of 2/12/18

--- home/assets/php/action.php
+++ home/assets/php/action.php
@@ -1,10 +1,10 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?><br>
-Your category is: <?php echo $_GET["category"]; ?><br>
-Your message to the server is: <?php echo $_GET["message"]; ?><br>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your category is: <?php echo $_POST["category"]; ?><br>
+Your message to the server is: <?php echo $_POST["message"]; ?><br>
 
 </body>
 </html>
