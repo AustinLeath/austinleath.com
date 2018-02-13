35cd99e6b74856e8415f8c5a1b0565a259f25734
Austin Leath <gameking2783@gmail.com>
2018-02-12 22:54:10 -0600

    Current version as of 2/12/18

--- home/assets/php/action.php
+++ home/assets/php/action.php
@@ -28,29 +28,29 @@
 									<li>
 								<a href="">Other Information</a>
 								  <ul>
-									 <li><a href="../git/github.html">GitHub Code</a></li>
-									 <li><a href="tools.php">Development Tools</a></li>
-									 <li><a href="rcs/index.php">Rileigh Sayen</a></li>
-									 <li><a href="info/aboutdeveloper.html">About Me</a></li>
-									 <li><a href="vrsenal/index.php">VRsenal</a></li>
-									 <li><a href="#">Return Home</a></li>
+									 <li><a href="../../../git/github.html">GitHub Code</a></li>
+									 <li><a href="../../../tools.php">Development Tools</a></li>
+									 <li><a href="../../../rcs/index.php">Rileigh Sayen</a></li>
+									 <li><a href="../../../info/aboutdeveloper.html">About Me</a></li>
+									 <li><a href="../../../vrsenal/index.php">VRsenal</a></li>
+									 <li><a href="../../../index.html">Return Home</a></li>
 								  </ul>
 							<!--DIVIDER-->
 										 <li>
 											<a href="">Legal</a>
 											 <ul>
-												 <li><a href="legal/apachelicense.php">Apache License</a></li>
-												 <li><a href="legal/mitlicense.php">MIT License</a></li>
+												 <li><a href="../../../legal/apachelicense.php">Apache License</a></li>
+												 <li><a href="../../../legal/mitlicense.php">MIT License</a></li>
 											 </ul>
 											</li>
 							 <!--DIVIDER-->
 									 <li>
 										 <a href="">Streaming</a>
 											 <ul>
-												<li><a href="streaming/gamesweplay.php">Games We Play</a></li>
+												<li><a href="../../../streaming/gamesweplay.php">Games We Play</a></li>
  												<li><a href="../../../streaming/streamerswewatch.php">Streamers We Watch</a></li>
- 												<li><a href="../../streaming/streaming.php">Streaming</a></li>
-												<li><a href="../streaming/form.php">Form</a></li>
+ 												<li><a href="../../../streaming/streaming.php">Streaming</a></li>
+												<li><a href="../../../streaming/form.php">Form</a></li>
 											 </ul>
 										  </li>
 							 <!--DIVIDER-->
@@ -91,8 +91,8 @@
 					</ul>
 					<ul class="copyright">
 						<li>&copy; austinleath.com / lethalgamestudios.ddns.net - All rights reserved. Lethal Game Studios&trade;</li>
-						<li><a href="legal/apachelicense.php" /a>Apache License</li>
-						<li><a href="legal/mitlicense.php" /a>MIT License</li>
+						<li><a href="../../../legal/apachelicense.php" /a>Apache License</li>
+						<li><a href="../../../legal/mitlicense.php" /a>MIT License</li>
 						<li><a href="javascript:history.go(-1)">Go Back</a></li>
 					</ul>
 				</footer>
