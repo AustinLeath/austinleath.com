bafe98c18a065f34378fadf3a0c6a8cef9873b1a
Austin Leath <gameking2783@gmail.com>
2018-02-19 20:00:21 -0600

    Current version as of 2/19/18

--- austinleath-win32-x64/resources/app/home/assets/js/cyclestyle.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
- document.getElementById("header").style.backgroundColor = "rgba(39, 40, 51, 0.965)";
- document.getElementById("background").style.backgroundColor = "#0375B4";
-};
--- austinleath-win32-x64/resources/app/home/assets/js/normalizestyle.js
+++
@@ -1,4 +0,0 @@
-function myFunction1() {
-  document.getElementById("header").style.backgroundColor = "rgba(39, 40, 51, 0.965)";
-  document.getElementById('background').style.backgroundColor = "inherit";
-};
