a50ff96dae3d37f01f013bd90c3d4b2622612ffd
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-23 10:48:51 -0600

    Current version as of 2/23/18

--- home/assets/js/cyclestyle.js
+++ home/assets/js/cyclestyle.js
@@ -1,4 +1,5 @@
 function myFunction() {
- document.getElementById("header").style.backgroundColor = "rgba(39, 40, 51, 0.965)";
  document.getElementById("background").style.backgroundColor = "#0375B4";
+ document.getElementById('btn1').disabled=true;
+ document.getElementById('btn2').disabled=false;
 };
--- home/assets/js/normalizestyle.js
+++ home/assets/js/normalizestyle.js
@@ -1,4 +1,5 @@
 function myFunction1() {
-  document.getElementById("header").style.backgroundColor = "rgba(39, 40, 51, 0.965)";
   document.getElementById('background').style.backgroundColor = "inherit";
+  document.getElementById('btn2').disabled=true;
+  document.getElementById('btn1').disabled=false;
 };
