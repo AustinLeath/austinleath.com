456a097220b47520d5e50e4592156241c82b04e2
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-02-27 11:53:52 -0600

    Current version as of 2/27/18

--- info.php
+++ info.php
@@ -11,72 +11,10 @@
 		<meta name='owner' content='Austin Leath'>
 		<link rel="shortcut icon" href="images/main/favicon.ico" />
 	</head>
-
 	<body>
-		<header id="header">
-			<h1 id="logo"><a href="">Austin Leath | Resume</a></h1>
-			<nav id="nav">
-				<ul>
-					<li>
-							<p id="timer"></p>
-							<li>
-							<button class="button small btnalign" id="btn1" type="button" onclick="myFunction()">Light Mode</button>
-							<li>
-							<button class="button small btnalign" id="btn2" type="button" onclick="myFunction1()">Dark Mode</button>
-							<li>
-						<a href="">Other Information</a>
-							<ul>
-							 <li><a href="git/github.html">GitHub Code</a></li>
-							 <li><a href="tools.php">Development Tools</a></li>
-							 <li><a href="rcs/index.php">Rileigh Sayen</a></li>
-							 <li><a href="info/aboutdeveloper.html">About Me</a></li>
-							 <li><a href="vrsenal/index.php">VRsenal</a></li>
-							 <li><a href="#">Return Home</a></li>
-							</ul>
-					<!--DIVIDER-->
-								 <li>
-									<a href="">Legal</a>
-									 <ul>
-										 <li><a href="legal/apachelicense.php">Apache License</a></li>
-										 <li><a href="legal/mitlicense.php">MIT License</a></li>
-									 </ul>
-									</li>
-					 <!--DIVIDER-->
-							 <li>
-								 <a href="">Streaming</a>
-									 <ul>
-										<li><a href="streaming/gamesweplay.php">Games We Play</a></li>
-										<li><a href="streaming/streamerswewatch.php">Streamers We Watch</a></li>
-										<li><a href="streaming/streaming.php">Streaming</a></li>
-										<li><a href="streaming/form.php">Form</a></li>
-									 </ul>
-									</li>
-					 <!--DIVIDER-->
-								 </li>
-								</ul>
-							 </li>
-					 <!-- Add Another Button Here -->
-				 </ul>
-			 </nav>
-		 </header>
-		 <div>
 		<?php
 			echo '<a href="index.html">Return Home</a>';
 			phpInfo();
 		?>
-	</div>
 	</body>
-		<script src="home/assets/js/time.js"></script>
-		<script src="home/assets/js/jquery.min.js"></script>
-		<script src="home/assets/js/jquery.scrolly.min.js"></script>
-		<script src="home/assets/js/jquery.dropotron.min.js"></script>
-		<script src="home/assets/js/jquery.scrollex.min.js"></script>
-		<script src="home/assets/js/skel.min.js"></script>
-		<script src="home/assets/js/util.js"></script>
-		<script src="home/assets/js/cycletxt.js"></script>
-		<script src="home/assets/js/normalizestyle.js"></script>
-		<script src="home/assets/js/cyclestyle.js"></script>
-		<!--[if lte IE 8]><script src="home/assets/js/ie/respond.min.js"></script><![endif]-->
-		<script src="home/assets/js/main.js"></script>
-	<!--Scripts-->
 </html>
