178aca49d9d873710e204a6213f301808ac78d6c
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-03-07 11:58:09 -0600

    Current version as of 3/7/18

--- home/assets/php/send_form_email.php
+++ home/assets/php/send_form_email.php
@@ -2,8 +2,8 @@
 if(isset($_POST['email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "Your email subject line";
+    $email_to = "gameking2783@gmail.com";
+    $email_subject = "FROM WEBSITE USER";
 
     function died($error) {
         // your error code can go here
