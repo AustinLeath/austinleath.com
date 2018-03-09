bbeda4ce365c4e9f8cea1494abcb68a83aabed69
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-03-09 12:45:22 -0600

    Current version as of 3/9/18

--- tools.php
+++ tools.php
@@ -152,6 +152,11 @@
 																		</div>
 																	</div>
 															</form>
+
+															<?php
+$homepage = file_get_contents('http://www.example.com/');
+echo $homepage;
+?>
 										</section>
 							</div>
 						</div>
