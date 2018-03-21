54446b4f8bb21671e2a89da96117a9a2b641de0a
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-03-21 16:31:53 -0500

    C

--- r6tsg.php
+++ r6tsg.php
@@ -757,32 +757,6 @@
 
 
 								</section>
-										<?php echo $_POST["rankfinal"]; ?>
-
-										<?php
-										$NoRankSelected = 0;
-										$CopperIV = 1299;
-										$CopperIII = 1399;
-										$CopperII = 1499;
-										$CopperI = 1599;
-										$BronzeIV = 1699;
-										$BronzeIII = 1799;
-										$BronzeII = 1899;
-										$BronzeI = 1999;
-										$SilverIV = 2099;
-										$SilverIII = 2199;
-										$SilverII = 2299
-										$SilverI = 2399;
-										$GoldIV = 2499;
-										$GoldIII = 2699;
-										$GoldII = 2899;
-										$GoldI = 3099;
-										$PlatinumIII = 3299;
-										$PlatinumII = 3699;
-										$PlatinumI = 4099;
-										$diamond = 4499;
-										print_r $_POST["rankfinal"];
-										?>
 					</div>
 			</div>
 			<iframe class="r6db" src="https://r6db.com/"></iframe>
