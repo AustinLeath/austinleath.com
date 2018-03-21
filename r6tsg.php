8aee7110870bf1181838479ec5d0ab8f5dc83453
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-03-21 13:07:54 -0500

    Current version as of 3/21/18

--- r6tsg.php
+++ r6tsg.php
@@ -754,6 +754,38 @@
 
 															Your current rank is: <?php echo $_POST["rank"]; ?><br>
 															The number of matches you need to win to get to <?php echo $_POST["rankfinal"]; ?> is: <?php echo $_POST["rankfinal"]; ?><br>
+
+
+
+															<?php
+															$NoRankSelected = 0;
+
+															$CopperIV = 1299;
+															$CopperIII = 1399;
+															$CopperII = 1499;
+															$CopperI = 1599
+															$BronzeIV = 1699;
+															$BronzeIII = 1799;
+															$BronzeII = 1899;
+															$BronzeI = 1999;
+															$SilverIV = 2099;
+															$SliverIII = 2199;
+															$SilverII = 2299
+															$SilverI = 2399;
+															$GoldIV = 2499;
+															$GoldIII = 2699;
+															$GoldII = 2899;
+															$GoldI = 3099;
+															$PlatinumIII = 3299;
+															$PlatinumII = 3699;
+															$PlatinumI = 4099;
+															$Diamond = 4499;
+
+															Your current MMR per match is: echo $_POST["MMR"];
+
+
+															?>
+
 								</section>
 					</div>
 			</div>
