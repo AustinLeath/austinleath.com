6857ccde2ff59e718b6e680bf644f6765a9a31ec
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-03-26 14:44:11 -0500

    Current version as of 3/26/18

--- home/assets/js/loadjson.js
+++ home/assets/js/loadjson.js
@@ -3,31 +3,7 @@ $(function() {
   $.getJSON('tsg_lethal.json', function(data) {
     $.each(data.aliases, function(i, f) {
       var tblRow = "<tr>" + "<td>" + f.name + "</td>" + "<td>" + f.created_at + "</td>" + "</tr>"
-      $(tblRow).appendTo("#userdata tbody");
+      $(tblRow).appendTo("#tsg_lethal tbody");
     });
   });
 });
-
-//sort of works
-
-//$(function() {
-//  var lastPlayed = [];
-//  $.getJSON('tsg_lethal.json', function(data) {
-//    $.each(data.lastPlayed, function(i, f) {
-//      var paragraph = "<p>" + f.casual + "</p>"
-//      $(paragraph).appendTo("#userdata p");
-//    });
-//  });
-//});
-
-//just incase
-//$(function() {
-  //var platform = [];
-  //$.getJSON('tsg_lethal.json', function(data) {
-    //$.each(data.platform, function(i, f) {
-      //var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
-        //"<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + "<td>" + f.roll + "</td>" + "</tr>"
-      //$(tblRow).appendTo("#userdata tbody");
-    //});
-  //});
-//});
