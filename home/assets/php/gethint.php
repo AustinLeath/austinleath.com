760d17ac88e92e6982f6a746ceb2cbbbf4b40d6f
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-03-26 10:00:36 -0500

    Current version as of 3/26/18

--- home/assets/php/gethint.php
+++ home/assets/php/gethint.php
@@ -1,37 +1,37 @@
 <?php
 // Array with names
 // First and last names as well as just last names
-$a[] = 'Ben Pearson ---> TSG.Slurgus <a href="www.google.com">R6DB</a>';
-$a[] = 'Pearson ---> TSG.Slurgus <a href="www.google.com">R6DB</a>';
-$a[] = 'Ben Pearson ---> TSG.Slurgus2.0 <a href="www.google.com">R6DB</a>';
-$a[] = 'Pearson ---> TSG.Slurgus2.0 <a href="www.google.com">R6DB</a>';
-$a[] = 'Austin Leath ---> TSG.Lethal <a href="www.google.com">R6DB</a>';
-$a[] = 'Leath ---> TSG.Lethal <a href="www.google.com">R6DB</a>';
-$a[] = 'Jake Martin ---> TSG.Temp-0 <a href="www.google.com">R6DB</a>';
-$a[] = 'Martin ---> TSG.Temp-0 <a href="www.google.com">R6DB</a>';
-$a[] = 'Henry Jardine ---> TSG.Jiffy <a href="www.google.com">R6DB</a>';
-$a[] = 'Jardine ---> TSG.Jiffy <a href="www.google.com">R6DB</a>';
-$a[] = 'Matthew Osten ---> TSG.Struck <a href="www.google.com">R6DB</a>';
-$a[] = 'Osten ---> TSG.Struck <a href="www.google.com">R6DB</a>';
-$a[] = 'Logan Romich ---> TSG.Phishz <a href="www.google.com">R6DB</a>';
-$a[] = 'Romich ---> TSG.Phishz <a href="www.google.com">R6DB</a>';
-$a[] = 'Brennan Carr ---> TSG.Pluxt <a href="www.google.com">R6DB</a>';
-$a[] = 'Carr ---> TSG.Pluxt <a href="www.google.com">R6DB</a>';
-$a[] = 'Dalton Cady ---> TSG.Prince <a href="www.google.com">R6DB</a>';
-$a[] = 'Cady ---> TSG.Prince <a href="www.google.com">R6DB</a>';
-$a[] = 'Brandon Gonzalez ---> TSG.Kazuhira <a href="www.google.com">R6DB</a>';
-$a[] = 'Gonzalez ---> TSG.Kazuhira <a href="www.google.com">R6DB</a>';
+$a[] = 'Ben Pearson ---> TSG.Slurgus <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Pearson ---> TSG.Slurgus <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Ben Pearson ---> TSG.Slurgus2.0 <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Pearson ---> TSG.Slurgus2.0 <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Austin Leath ---> TSG.Lethal <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Leath ---> TSG.Lethal <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Jake Martin ---> TSG.Temp-0 <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Martin ---> TSG.Temp-0 <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Henry Jardine ---> TSG.Jiffy <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Jardine ---> TSG.Jiffy <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Matthew Osten ---> TSG.Struck <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Osten ---> TSG.Struck <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Logan Romich ---> TSG.Phishz <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Romich ---> TSG.Phishz <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Brennan Carr ---> TSG.Pluxt <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Carr ---> TSG.Pluxt <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Dalton Cady ---> TSG.Prince <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Cady ---> TSG.Prince <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Brandon Gonzalez ---> TSG.Kazuhira <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'Gonzalez ---> TSG.Kazuhira <a href="https://www.google.com" target="_blank">R6DB</a>';
 //usernames
-$a[] = 'TSG.Slurgus <a href="https://www.google.com">R6DB</a>';
-$a[] = 'TSG.Slurgus2.0 <a href="www.google.com">R6DB</a>';
-$a[] = 'TSG.Lethal <a href="www.google.com">R6DB</a>';
-$a[] = 'TSG.Temp-0 <a href="www.google.com">R6DB</a>';
-$a[] = 'TSG.Jiffy <a href="www.google.com">R6DB</a>';
-$a[] = 'TSG.Struck <a href="www.google.com">R6DB</a>';
-$a[] = 'TSG.Phishz <a href="www.google.com">R6DB</a>';
-$a[] = 'TSG.Prince <a href="www.google.com">R6DB</a>';
-$a[] = 'TSG.Kazuhira <a href="www.google.com">R6DB</a>';
-$a[] = 'TSG.Pluxt <a href="www.google.com">R6DB</a>';
+$a[] = 'TSG.Slurgus <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Slurgus2.0 <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Lethal <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Temp-0 <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Jiffy <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Struck <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Phishz <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Prince <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Kazuhira <a href="https://www.google.com" target="_blank">R6DB</a>';
+$a[] = 'TSG.Pluxt <a href="https://www.google.com" target="_blank">R6DB</a>';
 
 // get the q parameter from URL
 $q = $_REQUEST["q"];
