e9dcf233e9a1efcf8f349c092e9f166c2be643b0
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-03-28 11:55:44 -0500

    Current version as of 3/28/18

--- playerdata/playerdata.php
+++ playerdata/playerdata.php
@@ -9,9 +9,9 @@
 		<meta name="author" content="Austin Leath">
 		<meta name='copyright' content='Lethal Game Studios'>
 		<meta name='owner' content='Austin Leath'>
-		<link rel="shortcut icon" href="images/main/favicon.ico" />
+		<link rel="shortcut icon" href="../images/main/favicon.ico" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="home/assets/css/main.css" />
+		<link rel="stylesheet" href="../home/assets/css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
@@ -32,27 +32,27 @@
 									<li>
 								<a href="">Other Information</a>
 								  <ul>
-									 <li><a href="git/github.html">GitHub Code</a></li>
-									 <li><a href="tools.php">Development Tools</a></li>
+									 <li><a href="../git/github.html">GitHub Code</a></li>
+									 <li><a href="../tools.php">Development Tools</a></li>
 									 <li><a href="">Legal</a>
 										 <ul>
-											 <li><a href="legal/mitlicense.php">MIT License</a></li>
-											 <li><a href="legal/apachelicense.php">Apache License</a></li>
+											 <li><a href="../legal/mitlicense.php">MIT License</a></li>
+											 <li><a href="../legal/apachelicense.php">Apache License</a></li>
 										 </ul>
 									 </li>
-									 <li><a href="rcs/index.php">Rileigh Sayen</a></li>
-									 <li><a href="info/aboutdeveloper.html">About Me</a></li>
-									 <li><a href="vrsenal/index.php">VRsenal</a></li>
-									 <li><a href="index.html">Return Home</a></li>
+									 <li><a href="../rcs/index.php">Rileigh Sayen</a></li>
+									 <li><a href="../info/aboutdeveloper.html">About Me</a></li>
+									 <li><a href="../vrsenal/index.php">VRsenal</a></li>
+									 <li><a href="../index.html">Return Home</a></li>
 								  </ul>
 							 <!--DIVIDER-->
 									 <li>
 										 <a href="">Streaming</a>
 											 <ul>
-												<li><a href="streaming/gamesweplay.php">Games We Play</a></li>
- 												<li><a href="streaming/streamerswewatch.php">Streamers We Watch</a></li>
- 												<li><a href="streaming/streaming.php">Streaming</a></li>
-												<li><a href="streaming/form.php">Form</a></li>
+												<li><a href="../streaming/gamesweplay.php">Games We Play</a></li>
+ 												<li><a href="../streaming/streamerswewatch.php">Streamers We Watch</a></li>
+ 												<li><a href="../streaming/streaming.php">Streaming</a></li>
+												<li><a href="../streaming/form.php">Form</a></li>
 											 </ul>
 										  </li>
 							 <!--DIVIDER-->
@@ -66,7 +66,7 @@
 			<!-- Main -->
 				<div id="main" class="wrapper style1">
 					<div class="container">
-						<a class="image fit"><img src="images/r6.jpg" alt="" /></a>
+						<a class="image fit"><img src="../images/r6.jpg" alt="" /></a>
 										<!-- Content -->
 													<section id="content">
 													<!--DIVIDER-->
@@ -255,36 +255,36 @@
 				</ul>
 				<ul class="copyright">
 					<li>&copy; austinleath.com - All rights reserved. Lethal Game Studios&trade;</li>
-					<li><a href="legal/apachelicense.php" /a>Apache License</li>
-					<li><a href="legal/mitlicense.php" /a>MIT License</li>
+					<li><a href="../legal/apachelicense.php" /a>Apache License</li>
+					<li><a href="../legal/mitlicense.php" /a>MIT License</li>
 					<li><a href="javascript:history.go(-1)">Go Back</a></li>
 				</ul>
 			</footer>
 	</div>
 		<!-- Scripts -->
-		  <script src="home/assets/js/time.js"></script>
-			<script src="home/assets/js/jquery.min.js"></script>
-			<script src="home/assets/js/jquery.scrolly.min.js"></script>
-			<script src="home/assets/js/jquery.dropotron.min.js"></script>
-			<script src="home/assets/js/jquery.scrollex.min.js"></script>
-			<script src="home/assets/js/skel.min.js"></script>
-			<script src="home/assets/js/util.js"></script>
-			<script src="home/assets/js/cycletxt.js"></script>
-			<script src="home/assets/js/normalizestyle.js"></script>
-			<script src="home/assets/js/cyclestyle.js"></script>
-			<script src="home/assets/js/showanswer.js"></script>
+		  <script src="../home/assets/js/time.js"></script>
+			<script src="../home/assets/js/jquery.min.js"></script>
+			<script src="../home/assets/js/jquery.scrolly.min.js"></script>
+			<script src="../home/assets/js/jquery.dropotron.min.js"></script>
+			<script src="../home/assets/js/jquery.scrollex.min.js"></script>
+			<script src="../home/assets/js/skel.min.js"></script>
+			<script src="../home/assets/js/util.js"></script>
+			<script src="../home/assets/js/cycletxt.js"></script>
+			<script src="../home/assets/js/normalizestyle.js"></script>
+			<script src="../home/assets/js/cyclestyle.js"></script>
+			<script src="../home/assets/js/showanswer.js"></script>
 			<!--[if lte IE 8]><script src="home/assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="home/assets/js/main.js"></script>
+			<script src="../home/assets/js/main.js"></script>
 				<!--DIVIDER-->
-			<script src="home/assets/js/loadjson/tsg_lethal.js"></script>
-			<script src="home/assets/js/loadjson/tsg_tempo.js"></script>
-			<script src="home/assets/js/loadjson/tsg_slurgus.js"></script>
-			<script src="home/assets/js/loadjson/tsg_slurgus2.js"></script>
-			<script src="home/assets/js/loadjson/tsg_jiffy.js"></script>
-			<script src="home/assets/js/loadjson/tsg_pluxt.js"></script>
-			<script src="home/assets/js/loadjson/tsg_prince.js"></script>
-			<script src="home/assets/js/loadjson/tsg_struck.js"></script>
-			<script src="home/assets/js/loadjson/tsg_kazuhira.js"></script>
-			<script src="home/assets/js/loadjson/tsg_phishz.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_lethal.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_tempo.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_slurgus.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_slurgus2.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_jiffy.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_pluxt.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_prince.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_struck.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_kazuhira.js"></script>
+			<script src="../home/assets/js/loadjson/tsg_phishz.js"></script>
 	</body>
 </html>
