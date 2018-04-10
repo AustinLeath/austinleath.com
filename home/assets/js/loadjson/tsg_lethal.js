61f8fc6669b1c0372c23e68ab884204f31692eba
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-04-10 10:34:10 -0500

    Current version as of 4/10/18

--- home/assets/js/loadjson/tsg_lethal.js
+++ home/assets/js/loadjson/tsg_lethal.js
@@ -1,6 +1,6 @@
 $(function() {
   var aliases = [];
-  $.getJSON('tsg_lethal.json', function(data) {
+  $.getJSON('../tsg_lethal.json', function(data) {
     $.each(data.aliases, function(i, f) {
       var tblRow = "<tr>" + "<td>" + f.name + "</td>" + "<td>" + f.created_at + "</td>" + "</tr>"
       $(tblRow).appendTo("#tsg_lethal tbody");
