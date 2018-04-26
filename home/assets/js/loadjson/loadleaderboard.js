977c7115cc1f59d0c535537d1ae08014b03c0b4e
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-04-26 14:30:50 -0500

    Current

--- home/assets/js/loadjson/loadleaderboard.js
+++ home/assets/js/loadjson/loadleaderboard.js
@@ -1,7 +1,7 @@
 $(function() {
   var aliases = [];
   $.getJSON('leaderboard.json', function(data) {
-    $.each(name, function(i, f) {
+    $.each(data.name, function(i, f) {
       var tblRow = "<tr>" + "<td>" + f.name + "</td>" + "<td>" + f.placement + "</td>" + "<td>" + f.name + "</td>" + "</tr>"
       $(tblRow).appendTo("#leaderboard tbody");
     });
