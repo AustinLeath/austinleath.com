3cf3eb850da774fca1d16a169cb1101d24f30ef0
AustinLeath <austinleath18@gmail.com>
2018-04-29 05:02:54 -0500

    Current

--- r6tsg.php
+++ r6tsg.php
@@ -390,7 +390,7 @@
 						id = data.id;
 						mmr = data.rank.ncsa.mmr;
 						round = Math.floor(mmr);
-						document.getElementById("767374e1-b218-42ff-98c6-3f78553db5e9").innerHTML = '<a href="https://r6db.com/player/' + id + '" target="_blank">' + name + '</a>' + ' MMR: ' mmr + ' KILLS: ' rankedkills;
+						document.getElementById("767374e1-b218-42ff-98c6-3f78553db5e9").innerText = '<a href="https://r6db.com/player/' + id + '" target="_blank">' + name + '</a>' + ' MMR: ' mmr + ' KILLS: ' rankedkills;
 				});
 		});
 		</script>
