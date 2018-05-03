0a1b90e2f9ccaf8ed88a41b3556c8c6a5b9ee004
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-05-03 13:44:43 -0500

    Current

--- tools.php
+++ tools.php
@@ -109,8 +109,7 @@
 										</tbody>
 										<tfoot>
 											<tr>
-												<td colspan="2"></td>
-												<td>Current as of <?php echo date("F");?> of <?php echo date("Y");?></td>
+												<td colspan="3">This information is current as of <?php echo date("F");?>, <?php echo date("j");?> of <?php echo date("Y");?></td>
 											</tr>
 										</tfoot>
 									</table>
