a9f57dade0942eac756e2bfe920e0b0b44d324e7
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-05-03 13:18:57 -0500

    Current version as of 5/3/18

--- tools.php
+++ tools.php
@@ -87,39 +87,29 @@
 										</thead>
 										<tbody>
 											<tr>
-												<td>Windows Release</td>
+												<td>Rainbow Six Siege | MMR Calculator: Windows Release</td>
 												<td><a class="button icon fa-download" href="https://github.com/AustinLeath/mmrcalculator/releases/download/v1.0.0/MMR.Calculator.Windows.zip" download>Download Project</a></td>
 												<td>Version 1.0.0</td>
 											</tr>
 											<tr>
-												<td>Mac Release</td>
+												<td>Rainbow Six Siege | MMR Calculator: Mac Release</td>
 												<td><a class="button icon fa-download" href="https://github.com/AustinLeath/mmrcalculator/releases/download/v1.0.0/MMR.Calculator.Mac.zip" download>Download Project</a></td>
 												<td>Version 1.0.0</td>
 											</tr>
 											<tr>
-												<td>Linux Release (x64)</td>
+												<td>Rainbow Six Siege | MMR Calculator: Linux Release (x64)</td>
 												<td><a class="button icon fa-download" href="https://github.com/AustinLeath/mmrcalculator/releases/download/v1.0.0/MMR.Calculator.Linux.x64.zip" download>Download Project</a></td>
 												<td>Version 1.0.0</td>
 											</tr>
 											<tr>
-												<td>Linux Release (Armv7l)</td>
+												<td>Rainbow Six Siege | MMR Calculator: Linux Release (Armv7l)</td>
 												<td><a class="button icon fa-download" href="https://github.com/AustinLeath/mmrcalculator/releases/download/v1.0.0/MMR.Calculator.Linux.Armv7l.zip" download>Download Project</a></td>
-												<td>Version 1.0.0</td>
-											</tr>
-											<tr>
-												<td>Site Source Code</td>
-												<td><a class="button icon fa-download" href="https://www.github.com/AustinLeath/main/archive/master.zip" download>Download Project</a></td>
-												<td>Version 1.0.0</td>
-											</tr>
-											<tr>
-												<td>Rainbow Six Siege | MMR Calculator</td>
-												<td><a class="button icon fa-download" href="https://www.github.com/AustinLeath/MMRcalculator/archive/master.zip" download>Download Project</a></td>
 												<td>Version 1.0.0</td>
 											</tr>
 										</tbody>
 										<tfoot>
 											<tr>
-												<td colspan="2"></td>
+												<td colspan="3"></td>
 												<td>Current as of <?php echo date("F");?> of <?php echo date("Y");?></td>
 											</tr>
 										</tfoot>
