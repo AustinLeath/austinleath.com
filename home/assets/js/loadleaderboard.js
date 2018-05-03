a383237b96ed561945fd3cdc56778f964550d9d4
AustinLeath <austinleath18@gmail.com>
2018-05-02 20:14:53 -0500

    Current version as of 5/2/18

--- home/assets/js/loadleaderboard.js
+++ home/assets/js/loadleaderboard.js
@@ -1,6 +1,6 @@
 $.getJSON('leaderboard.json', (data) => {
     const tableData = data.reduce((pre, curr) => {
-      return pre + `<tr><td>${curr.placement}</td><td><a href="https://www.r6db.com/players/${curr.id}">${curr.name}</a></td><td>${curr.value}</td></tr>`;
+      return pre + `<tr><td>${curr.placement}</td><td><a href="https://www.r6db.com/player/${curr.id}">${curr.name}</a></td><td>${curr.value}</td></tr>`;
     }, '');
 
     $(tableData).appendTo('#leaderboard tbody');
