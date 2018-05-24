a9c100bc36c22a4588fc225c341c4e95606b6462
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-05-24 13:08:54 -0500

    CHANGES MADE DUE TO THE R6DB SHUTDOWN

--- playerdata.php
+++ playerdata.php
@@ -1,147 +1,55 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>R6S | Player Data</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta name="keywords" content="HTML,CSS,XML,JavaScript,Web,Engineer,Resume,CSS3,HTML5,Austin,Leath,Lethal,Game,Studios,LethalGameStudios">
-		<meta name="description" content="This page displays stored player data.">
-		<meta name="author" content="Austin Leath">
-		<meta name='copyright' content='Lethal Game Studios'>
-		<meta name='owner' content='Austin Leath'>
-		<link rel="shortcut icon" href="images/main/favicon.ico" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="home/assets/css/main.css" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-	</head>
-	<body id="background">
-		<div id="page-wrapper">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>R6RC - Player Data</title>
+<meta name="apple-mobile-web-app-title" content="R6RC">
+<meta name="application-name" content="R6RC">
+<meta name="theme-color" content="#191919">
+<meta id="meta_description" name="description" content="This page is unavailable for the foreseeable future due to the R6DB API shutdown.">
+<meta name="keywords" content="R6RC, Player, Data" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+<style>
+  body {
+      background: #181b1f;
+      color: #FFF;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif,
+          Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+      text-align: center;
+      margin: 0;
+  }
+  #page {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+  }
+  .item {
+      flex: 1;
+        max-width: 50%;
+  }
+  h1 {
+      font-weight: 500;
+  }
+  span {
+      color: #6f7376;
+      font-size: 14px;
+  }
+  a {
+      color: #A4A9AD;
+  }
+  a:hover, a:focus {
+      color: #FFF;
+  }
+</style>
+<body>
+    <div id="page">
+        <div class="item">
+            <h1>This page is unavailable for the foreseeable future due to the <a href="https://medium.com/@r6db/quick-update-on-the-shutdown-3c761dcc24e6" target="_blank">R6DB API</a> shutdown.</h1>
+            <span>Return <a href="/index.html">Home</a> or return to the <a href="javascript:history.go(-1)">previous page</a>.</span>
+        </div>
+    </div>
+</body>
 
-			<!-- Header -->
-				<header id="header">
-					<h1 id="logo"><a href="">R6S | Player Data</a></h1>
-					<nav id="nav">
-						<ul>
-							<li>
-									<p id="timer"></p>
-									<li>
-									<button class="button small btnalign" id="btn1" type="button" onclick="myFunction()">Light Mode</button>
-									<li>
-									<button class="button small btnalign" id="btn2" type="button" onclick="myFunction1()">Dark Mode</button>
-									<li>
-								<a href="">Other Information</a>
-								  <ul>
-									 <li><a href="git/github.html">GitHub Code</a></li>
-									 <li><a href="tools.php">Development Tools</a></li>
-									 <li><a href="">Legal</a>
-										 <ul>
-											 <li><a href="legal/mitlicense.php">MIT License</a></li>
-											 <li><a href="legal/apachelicense.php">Apache License</a></li>
-										 </ul>
-									 </li>
-									 <li><a href="rcs/index.php">Rileigh Sayen</a></li>
-									 <li><a href="info/aboutdeveloper.html">About Me</a></li>
-									 <li><a href="vrsenal/index.php">VRsenal</a></li>
-									 <li><a href="index.html">Return Home</a></li>
-								  </ul>
-							 <!--DIVIDER-->
-									 <li>
-										 <a href="">Streaming</a>
-											 <ul>
-												<li><a href="streaming/gamesweplay.php">Games We Play</a></li>
- 												<li><a href="streaming/streamerswewatch.php">Streamers We Watch</a></li>
- 												<li><a href="streaming/streaming.php">Streaming</a></li>
-												<li><a href="streaming/form.php">Form</a></li>
-											 </ul>
-										  </li>
-							 <!--DIVIDER-->
-										 </li>
-									  </ul>
-							     </li>
-						   <!-- Add Another Button Here -->
-						 </ul>
-					 </nav>
-	  		 </header>
-			<!-- Main -->
-				<div id="main" class="wrapper style1">
-					<div class="container">
-						<a class="image fit"><img src=" images/r6.jpg" alt="" /></a>
-										<!-- Content -->
-													<section id="content">
-													<!--DIVIDER-->
-														<h1>Username History for: TSG.Lethal</h1>
-															<table id="tsg_lethal">
-																<thead>
-																	<tr>
-																		<th>Username History</th>
-																		<th>Date of Change</th>
-																	</tr>
-																</thead>
-																<tbody>
-																</tbody>
-																<tfoot>
-																	<tr>
-																		<td colspan="2">This information is current as of <?php echo date("F");?>, <?php echo date("j");?> of <?php echo date("Y");?></td>
-																	</tr>
-																</tfoot>
-															</table>
-
-															<h1>Rainbow Six: Siege Global Leaderboard</h1>
-																<table id="leaderboard">
-																	<thead>
-																		<tr>
-																			<th>Place</th>
-																			<th>Username</th>
-																			<th>Rating</th>
-																		</tr>
-																	</thead>
-																	<tbody>
-																	</tbody>
-																	<tfoot>
-																		<tr>
-																			<td colspan="3">This information is current as of <?php echo date("F");?>, <?php echo date("j");?> of <?php echo date("Y");?></td>
-																		</tr>
-																	</tfoot>
-																</table>
-														<!--Divider-->
-													</section>
-								</section>
-						 </div>
-				</div>
-			<footer id="footer">
-				<ul class="icons">
-					<li><a href="https://twitter.com/austinzleath" target="_blank" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
-					<li><a href="https://www.facebook.com/AustinZacharyLeath" target="_blank" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
-					<li><a href="https://www.linkedin.com/in/austin-leath-30a39a106/" target="_blank" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
-					<li><a href="https://www.instagram.com/austin_leath/" target="_blank" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
-					<li><a href="https://github.com/AustinLeath/main" target="_blank" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
-					<li><a href="mailto:austinleath18@gmail.com" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
-				</ul>
-				<ul class="copyright">
-					<li>&copy; austinleath.com - All rights reserved. Lethal Game Studios&trade;</li>
-					<li><a href="legal/apachelicense.php" /a>Apache License</li>
-					<li><a href="legal/mitlicense.php" /a>MIT License</li>
-					<li><a href="javascript:history.go(-1)">Go Back</a></li>
-				</ul>
-			</footer>
-	</div>
-		<!-- Scripts -->
-		  <script src=" home/assets/js/time.js"></script>
-			<script src=" home/assets/js/jquery.min.js"></script>
-			<script src=" home/assets/js/jquery.scrolly.min.js"></script>
-			<script src=" home/assets/js/jquery.dropotron.min.js"></script>
-			<script src=" home/assets/js/jquery.scrollex.min.js"></script>
-			<script src=" home/assets/js/skel.min.js"></script>
-			<script src=" home/assets/js/util.js"></script>
-			<script src=" home/assets/js/cycletxt.js"></script>
-			<script src=" home/assets/js/normalizestyle.js"></script>
-			<script src=" home/assets/js/cyclestyle.js"></script>
-			<script src=" home/assets/js/showanswer.js"></script>
-			<script src=" home/assets/js/loadleaderboard.js"></script>
-			<!--[if lte IE 8]><script src="home/assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src=" home/assets/js/main.js"></script>
-			<!--DIVIDER-->
-			<script src=" home/assets/js/loadjson/tsg_lethal.js"></script>
-	</body>
 </html>
