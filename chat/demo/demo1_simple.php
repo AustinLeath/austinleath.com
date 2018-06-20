9ca4e71ec79bc5be2b0b3c5b5fc6c43b067c4154
AustinLeath <austinleath18@gmail.com>
2018-06-19 22:23:44 -0500

    Current version as of 6/19/18

--- chat/demo/demo1_simple.php
+++ chat/demo/demo1_simple.php
@@ -13,20 +13,9 @@ $chat = new phpFreeChat($params);
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title>phpFreeChat demo</title>
   </head>
-    
+
   <body>
     <?php $chat->printChat(); ?>
-
-<?php
-  // print the current file
-  echo "<h2>The source code</h2>";
-  $filename = __FILE__;
-  echo "<p><code>".$filename."</code></p>";
-  echo "<pre style=\"margin: 0 50px 0 50px; padding: 10px; background-color: #DDD;\">";
-  $content = file_get_contents($filename);
-  highlight_string($content);
-  echo "</pre>";
-?>
   </body>
-  
+
 </html>
