c9801712d61158f29b99b5dc32e8684c7584b119
AustinLeath <austinleath18@gmail.com>
2018-10-14 02:04:41 -0500

    Current version as of 10/14/18

--- home/assets/php/gethint.php
+++ home/assets/php/gethint.php
@@ -11,35 +11,35 @@
 //$a[] = 'Martin -> TSG.Temp-0 <a href="https://r6tab.com/ca54e49a-76f2-4290-8e11-e36827caccb0" target="_blank">R6Tab</a>';
 //$a[] = 'Henry Jardine -> TSG.Jiffy <a href="https://r6tab.com/f561393f-63bb-4332-b71b-cbb1665b46ed" target="_blank">R6Tab</a>';
 //$a[] = 'Jardine -> TSG.Jiffy <a href="https://r6tab.com/f561393f-63bb-4332-b71b-cbb1665b46ed" target="_blank">R6Tab</a>';
-$a[] = 'Matthew Osten -> TSG.Struck <a href="https://r6tab.com/fa845839-343e-4c45-abed-979c3716a258" target="_blank">R6Tab</a>';
-$a[] = 'Osten -> TSG.Struck <a href="https://r6tab.com/fa845839-343e-4c45-abed-979c3716a258" target="_blank">R6Tab</a>';
-$a[] = 'Logan Romich -> TSG.Phishz <a href="https://r6tab.com/17541266-d840-4044-aa46-8f1e3edc7c5e" target="_blank">R6Tab</a>';
-$a[] = 'Romich -> TSG.Phishz <a href="https://r6tab.com/17541266-d840-4044-aa46-8f1e3edc7c5e" target="_blank">R6Tab</a>';
-$a[] = 'Brennan Carr -> TSG.Pluxt <a href="https://r6tab.com/ef1ed143-4db1-44b0-b0cd-796cf52dddbe" target="_blank">R6Tab</a>';
-$a[] = 'Carr -> TSG.Pluxt <a href="https://r6tab.com/ef1ed143-4db1-44b0-b0cd-796cf52dddbe" target="_blank">R6Tab</a>';
-$a[] = 'Dalton Cady -> TSG.Prince <a href="https://r6tab.com/80543135-bd7d-4c52-8c4d-c398cc35982a" target="_blank">R6Tab</a>';
-$a[] = 'Cady -> TSG.Prince <a href="https://r6tab.com/80543135-bd7d-4c52-8c4d-c398cc35982a" target="_blank">R6Tab</a>';
-$a[] = 'Brandon Gonzalez -> TSG.Kazuhira <a href="https://r6tab.com/840a9beb-4092-40ce-b22a-0284383e8dd5" target="_blank">R6Tab</a>';
-$a[] = 'Gonzalez -> TSG.Kazuhira <a href="https://r6tab.com/840a9beb-4092-40ce-b22a-0284383e8dd5" target="_blank">R6Tab</a>';
-$a[] = 'Chris Goins -> TSG.DEF3KT <a href="https://r6tab.com/5af9fdc8-db46-4ca5-b57d-6b10a962f07c" target="_blank">R6Tab</a>';
-$a[] = 'Goins -> TSG.DEF3KT <a href="https://r6tab.com/5af9fdc8-db46-4ca5-b57d-6b10a962f07c" target="_blank">R6Tab</a>';
-$a[] = 'Jacob LaCroix -> TSG.Legion <a href="https://r6tab.com/7c3185c0-74c9-440f-8f67-29cb3eaa4112" target="_blank">R6Tab</a>';
-$a[] = 'LaCroix -> TSG.Legion <a href="https://r6tab.com/7c3185c0-74c9-440f-8f67-29cb3eaa4112" target="_blank">R6Tab</a>';
-$a[] = 'Ryan Hughes -> TSG.Frontside <a href="https://r6tab.com/ce8303ce-6081-43a2-9ebd-e18953c7156a" target="_blank">R6Tab</a>';
-$a[] = 'Hughes -> TSG.Frontside <a href="https://r6tab.com/ce8303ce-6081-43a2-9ebd-e18953c7156a" target="_blank">R6Tab</a>';
+//$a[] = 'Matthew Osten -> TSG.Struck <a href="https://r6tab.com/fa845839-343e-4c45-abed-979c3716a258" target="_blank">R6Tab</a>';
+//$a[] = 'Osten -> TSG.Struck <a href="https://r6tab.com/fa845839-343e-4c45-abed-979c3716a258" target="_blank">R6Tab</a>';
+//$a[] = 'Logan Romich -> TSG.Phishz <a href="https://r6tab.com/17541266-d840-4044-aa46-8f1e3edc7c5e" target="_blank">R6Tab</a>';
+//$a[] = 'Romich -> TSG.Phishz <a href="https://r6tab.com/17541266-d840-4044-aa46-8f1e3edc7c5e" target="_blank">R6Tab</a>';
+//$a[] = 'Brennan Carr -> TSG.Pluxt <a href="https://r6tab.com/ef1ed143-4db1-44b0-b0cd-796cf52dddbe" target="_blank">R6Tab</a>';
+//$a[] = 'Carr -> TSG.Pluxt <a href="https://r6tab.com/ef1ed143-4db1-44b0-b0cd-796cf52dddbe" target="_blank">R6Tab</a>';
+//$a[] = 'Dalton Cady -> TSG.Prince <a href="https://r6tab.com/80543135-bd7d-4c52-8c4d-c398cc35982a" target="_blank">R6Tab</a>';
+//$a[] = 'Cady -> TSG.Prince <a href="https://r6tab.com/80543135-bd7d-4c52-8c4d-c398cc35982a" target="_blank">R6Tab</a>';
+//$a[] = 'Brandon Gonzalez -> TSG.Kazuhira <a href="https://r6tab.com/840a9beb-4092-40ce-b22a-0284383e8dd5" target="_blank">R6Tab</a>';
+//$a[] = 'Gonzalez -> TSG.Kazuhira <a href="https://r6tab.com/840a9beb-4092-40ce-b22a-0284383e8dd5" target="_blank">R6Tab</a>';
+//$a[] = 'Chris Goins -> TSG.DEF3KT <a href="https://r6tab.com/5af9fdc8-db46-4ca5-b57d-6b10a962f07c" target="_blank">R6Tab</a>';
+//$a[] = 'Goins -> TSG.DEF3KT <a href="https://r6tab.com/5af9fdc8-db46-4ca5-b57d-6b10a962f07c" target="_blank">R6Tab</a>';
+//$a[] = 'Jacob LaCroix -> TSG.Legion <a href="https://r6tab.com/7c3185c0-74c9-440f-8f67-29cb3eaa4112" target="_blank">R6Tab</a>';
+//$a[] = 'LaCroix -> TSG.Legion <a href="https://r6tab.com/7c3185c0-74c9-440f-8f67-29cb3eaa4112" target="_blank">R6Tab</a>';
+//$a[] = 'Ryan Hughes -> TSG.Frontside <a href="https://r6tab.com/ce8303ce-6081-43a2-9ebd-e18953c7156a" target="_blank">R6Tab</a>';
+//$a[] = 'Hughes -> TSG.Frontside <a href="https://r6tab.com/ce8303ce-6081-43a2-9ebd-e18953c7156a" target="_blank">R6Tab</a>';
 
 $a[] =
 'Austin Leath<table>
   <tbody>
     <tr>
       <td>Austin Leath</td>
-      <td><a class="button icon fa-download" href="https://r6tab.com/https://r6tab.com/767374e1-b218-42ff-98c6-3f78553db5e9" target="_blank">R6Tab</a></td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/767374e1-b218-42ff-98c6-3f78553db5e9" target="_blank">R6Tab</a></td>
     </tr>
   </tbody>
 </table>';
 
 $a[] =
-'<table>
+'Jake Martin<table>
   <tbody>
     <tr>
       <td>Jake Martin</td>
@@ -49,7 +49,7 @@ $a[] =
 </table>';
 
 $a[] =
-'<table>
+'Ben Pearson<table>
   <tbody>
     <tr>
       <td>Ben Pearson</td>
@@ -59,7 +59,7 @@ $a[] =
 </table>';
 
 $a[] =
-'<table>
+'Ben Pearson<table>
   <tbody>
     <tr>
       <td>Ben Pearson</td>
@@ -69,7 +69,7 @@ $a[] =
 </table>';
 
 $a[] =
-'<table>
+'Henry Jardine<table>
   <tbody>
     <tr>
       <td>Henry jardine</td>
@@ -79,90 +79,100 @@ $a[] =
 </table>';
 
 $a[] =
-'<table>
+'Matthew Osten<table>
   <tbody>
     <tr>
-      <td></td>
-      <td><a class="button icon fa-download" href="" target="_blank">R6Tab</a></td>
+      <td>Matthew Osten</td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/fa845839-343e-4c45-abed-979c3716a258" target="_blank">R6Tab</a></td>
     </tr>
   </tbody>
 </table>';
 
 $a[] =
-'<table>
+'Logan Romich<table>
   <tbody>
     <tr>
-      <td></td>
-      <td><a class="button icon fa-download" href="" target="_blank">R6Tab</a></td>
+      <td>Logan Romich</td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/17541266-d840-4044-aa46-8f1e3edc7c5e" target="_blank">R6Tab</a></td>
     </tr>
   </tbody>
 </table>';
 
 $a[] =
-'<table>
+'Brennan Carr<table>
   <tbody>
     <tr>
-      <td></td>
-      <td><a class="button icon fa-download" href="" target="_blank">R6Tab</a></td>
+      <td>Brennan Carr</td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/ef1ed143-4db1-44b0-b0cd-796cf52dddbe" target="_blank">R6Tab</a></td>
     </tr>
   </tbody>
 </table>';
 
 $a[] =
-'<table>
+'Dalton Cady<table>
   <tbody>
     <tr>
-      <td></td>
-      <td><a class="button icon fa-download" href="" target="_blank">R6Tab</a></td>
+      <td>Dalton Cady</td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/80543135-bd7d-4c52-8c4d-c398cc35982a" target="_blank">R6Tab</a></td>
     </tr>
   </tbody>
 </table>';
 
 $a[] =
-'<table>
+'Brandon Gonzalez<table>
   <tbody>
     <tr>
-      <td></td>
-      <td><a class="button icon fa-download" href="" target="_blank">R6Tab</a></td>
+      <td>Brandon Gonzalez</td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/840a9beb-4092-40ce-b22a-0284383e8dd5" target="_blank">R6Tab</a></td>
     </tr>
   </tbody>
 </table>';
 
 $a[] =
-'<table>
+'Chris Goins<table>
   <tbody>
     <tr>
-      <td></td>
-      <td><a class="button icon fa-download" href="" target="_blank">R6Tab</a></td>
+      <td>Chris Goins</td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/5af9fdc8-db46-4ca5-b57d-6b10a962f07c" target="_blank">R6Tab</a></td>
     </tr>
   </tbody>
 </table>';
 
 $a[] =
-'<table>
+'Jacob LaCroix<table>
   <tbody>
     <tr>
-      <td></td>
-      <td><a class="button icon fa-download" href="" target="_blank">R6Tab</a></td>
+      <td>Jacob LaCroix</td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/7c3185c0-74c9-440f-8f67-29cb3eaa4112" target="_blank">R6Tab</a></td>
+    </tr>
+  </tbody>
+</table>';
+
+$a[] =
+'Ryan Hughes<table>
+  <tbody>
+    <tr>
+      <td>Ryan Hughes</td>
+      <td><a class="button icon fa-download" href="https://r6tab.com/ce8303ce-6081-43a2-9ebd-e18953c7156a" target="_blank">R6Tab</a></td>
     </tr>
   </tbody>
 </table>';
 
 //usernames
 
-$a[] = 'TSG.Slurgus <a href="https://r6tab.com/8894b513-7067-473e-a001-e316104e32ea" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Slurgus2.0 <a href="https://r6tab.com/79a248d4-4ba8-4be0-ba42-e0e836307099" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Lethal <a href="https://r6tab.com/767374e1-b218-42ff-98c6-3f78553db5e9" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Temp-0 <a href="https://r6tab.com/ca54e49a-76f2-4290-8e11-e36827caccb0" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Jiffy <a href="https://r6tab.com/f561393f-63bb-4332-b71b-cbb1665b46ed" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Struck <a href="https://r6tab.com/fa845839-343e-4c45-abed-979c3716a258" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Phishz <a href="https://r6tab.com/17541266-d840-4044-aa46-8f1e3edc7c5e" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Prince <a href="https://r6tab.com/80543135-bd7d-4c52-8c4d-c398cc35982a" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Kazuhira <a href="https://r6tab.com/840a9beb-4092-40ce-b22a-0284383e8dd5" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Pluxt <a href="https://r6tab.com/ef1ed143-4db1-44b0-b0cd-796cf52dddbe" target="_blank">R6Tab</a>';
-$a[] = 'TSG.DEF3KT <a href="https://r6tab.com/5af9fdc8-db46-4ca5-b57d-6b10a962f07c" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Legion <a href="https://r6tab.com/7c3185c0-74c9-440f-8f67-29cb3eaa4112" target="_blank">R6Tab</a>';
-$a[] = 'TSG.Frontside <a href="https://r6tab.com/ce8303ce-6081-43a2-9ebd-e18953c7156a" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Slurgus <a href="https://r6tab.com/8894b513-7067-473e-a001-e316104e32ea" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Slurgus2.0 <a href="https://r6tab.com/79a248d4-4ba8-4be0-ba42-e0e836307099" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Lethal <a href="https://r6tab.com/767374e1-b218-42ff-98c6-3f78553db5e9" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Temp-0 <a href="https://r6tab.com/ca54e49a-76f2-4290-8e11-e36827caccb0" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Jiffy <a href="https://r6tab.com/f561393f-63bb-4332-b71b-cbb1665b46ed" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Struck <a href="https://r6tab.com/fa845839-343e-4c45-abed-979c3716a258" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Phishz <a href="https://r6tab.com/17541266-d840-4044-aa46-8f1e3edc7c5e" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Prince <a href="https://r6tab.com/80543135-bd7d-4c52-8c4d-c398cc35982a" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Kazuhira <a href="https://r6tab.com/840a9beb-4092-40ce-b22a-0284383e8dd5" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Pluxt <a href="https://r6tab.com/ef1ed143-4db1-44b0-b0cd-796cf52dddbe" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.DEF3KT <a href="https://r6tab.com/5af9fdc8-db46-4ca5-b57d-6b10a962f07c" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Legion <a href="https://r6tab.com/7c3185c0-74c9-440f-8f67-29cb3eaa4112" target="_blank">R6Tab</a>';
+//$a[] = 'TSG.Frontside <a href="https://r6tab.com/ce8303ce-6081-43a2-9ebd-e18953c7156a" target="_blank">R6Tab</a>';
 
 // get the q parameter from URL
 $q = $_REQUEST["q"];
