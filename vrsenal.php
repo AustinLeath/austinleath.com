0e3d871e3d00a1badfd8480f6e80f6e8c9d1647e
AustinLeath <austinleath18@gmail.com>
2019-01-19 00:58:43 -0600

    Current version as of 1/19/19

--- vrsenal.php
+++ vrsenal.php
@@ -43,6 +43,7 @@
 									 <li><a href="tools.php">Development Tools</a></li>
 									 <li><a href="aboutdeveloper.html">About Me</a></li>
 									 <li><a href="#">VRsenal</a></li>
+									 <li><a href="mmrcalculator.html">R6S | MMR Calculator</a></li>
 									 <li><a href="index.html">Return Home</a></li>
 								  </ul>
 							 <!--DIVIDER-->
