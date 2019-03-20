a7f4d884eb1579beb238b340f414200451b359de
Austin Leath <austinleath18@gmail.com>
2019-03-20 14:43:00 -0500

    Update vrsenal.php

--- vrsenal.php
+++ vrsenal.php
@@ -1,149 +1,150 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>Austin Leath | VRsenal</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta name="keywords" content="HTML,CSS,XML,JavaScript,Web,Engineer,Resume,CSS3,HTML5,Austin,Leath,Lethal,Game,Studios,LethalGameStudios">
-		<meta name="description" content="VRsenal is a single player VR game that is both an open world exploration game and physics simulation. Master the landscape as well as a variety of weapons that are laid before you!">
-		<meta name="author" content="Austin Leath">
-		<meta name='copyright' content='Lethal Game Studios'>
-		<meta name='owner' content='Austin Leath'>
-		<link rel="shortcut icon" href="images/favicon.ico" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="/assets/css/main.css" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111374271-2"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
-
-		  gtag('config', 'UA-111374271-2');
-		</script>
-	</head>
-	<body id="background">
-		<div id="page-wrapper">
-
-			<!-- Header -->
-				<header id="header">
-					<h1 id="logo"><a href="">Austin Leath | VRsenal</a></h1>
-					<nav id="nav">
-						<ul>
-							<li>
-								<p id="timer"></p>
-								<li>
-								<button class="button small btnalign" id="btn1" type="button" onclick="myFunction()">Light Mode</button>
-								<li>
-								<button class="button small btnalign" id="btn2" type="button" onclick="myFunction1()" disabled>Dark Mode</button>
-							<li>
-								<a href="">Other Information</a>
-								  <ul>
-									 <li><a href="tools.php">Development Tools</a></li>
-									 <li><a href="aboutdeveloper.html">About Me</a></li>
-									 <li><a href="#">VRsenal</a></li>
-									 <li><a href="mmrcalculator.html">R6S | MMR Calculator</a></li>
-									 <li><a href="index.html">Return Home</a></li>
-								  </ul>
-							 <!--DIVIDER-->
-										 </li>
-									  </ul>
-							     </li>
-						   <!-- Add Another Button Here -->
-						 </ul>
-					 </nav>
-	  		 </header>
-
-			<!-- Main -->
-				<div id="main" class="wrapper style1">
-					<div class="container">
-						<header class="major">
-							<h2>Austin Leath | VRsenal</h2>
-							<section>
-								<h5>A hub for all things VRsenal</h5>
-							</section>
-						</header>
-						<!-- Content -->
-							<section id="content">
-								<img class="image fit" id="image-1" src="images/img1.png" alt="" />
-								<img class="image fit" id="image-2" src="images/img2.png" alt="" />
-								<img class="image fit" id="image-3" src="images/img3.png" alt="" />
-								<img class="image fit" id="image-4" src="images/img4.png" alt="" />
-								<h3>VRsenal / HTC Vive supported / Independently developed</h3>
-								<p>VRsenal is a single player HTC Vive supported virtual reality game which is both an exploration game and a physics simulation. Explore the vast lands of VRsenal while interacting with random objects that are scattered throughout the many environments in which you will be playing in. VRsenal provides an assortment of weapons for you to interact with including both melee weapons, 3 hand guns and 4 Assault Rifles.</p>
-								<p>VRsenal is a single player VR game that is both an open world exploration game and physics simulation. Master the landscape as well as a variety of weapons that are laid before you!</p>
-								<header class="major">
-									<h3>Features and Functions</h3>
-								</header>
-										<!--Divider-->
-										<ul>
-										 <strong>Features</strong>
-											<li>Hand Guns</li>
-											<li>Assault Rifles</li>
-											<li>Paintball Guns</li>
-											<li>Bats</li>
-											<li>Swords</li>
-											<li>Knives</li>
-										</ul>
-										<ul>
-										 <strong>Environments</strong>
-											<li>Desert Environments</li>
-											<li>Arctic Environments</li>
-											<li>Urban Environments</li>
-											<li>Wilderness Environments</li>
-										</ul>
-									<!--Divider-->
-								</ul>
-							</section>
-								<header class="major">
-									<h3>Why I chose Early Access for VRsenal</h3>
-									</header>
-									<p>VRsenal is still in its early stages of development. So I would love to show the community what I have done so far and hear what everyone has to say as far as mechanics go. Things are a bit rusty at the moment but they will shape up soon!</p>
-										<h3>Approximately how long will this game be in Early Access?</h3>
-										<p>Originally I planned for this game to go straight to the market. After realizing that the amount of content was a tad bit low, I decided to go to early access. With the Early Access / initial release slated for July 4th, 2017. However, after starting my Senior year for High School I have realized that my focus needs to be on school.<br>For right now, VRsenal will be in early access for te forseeable future.</p>
-										<h3>How is the full version planned to differ from the Early Access version?</h3>
-										<p>There will be more weapons, game modes and places to explore / shoot up. I am also open to suggestions as far as content and overall game design goes.</p>
-										<h3>What is the current state of the Early Access version?</h3>
-										<p>The current state of the Early Access version is pre alpha. The game certainly isn't finished but with the help of the community this game will be a fun experience for everyone!</p>
-									<h3>Will the game be priced differently during and after Early Access?</h3>
-								<p>I plan to gradually raise the price leading up to the final release. That will only happen when most of the major content that I have planned is in place.</p>
-							<h3>How are you planning on involving the Community in your development process?</h3>
-						<p>I will keep the community involved by closely monitoring the forums and reading every suggestion from our players. We will let our players partake in beta builds so they can let us know what they like and what they dislike. I will also have a VRsenal community Discord where players can always stay in touch with me or my moderators.</p>
-						<header class="major">
-							<h3>VRsenal Trailer</h3>
-					 </header>
-					 <iframe class="image fit" width="1280" height="720" src="https://www.youtube.com/embed/mcwT7GV4144" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-					</div>
-				</div>
-			<!-- Footer -->
-				<footer id="footer">
-					<ul class="icons">
-						<li><a href="https://twitter.com/austinzleath" target="_blank" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="https://www.facebook.com/AustinZacharyLeath" target="_blank" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="https://www.linkedin.com/in/austin-leath-30a39a106/" target="_blank" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
-						<li><a href="https://www.instagram.com/austin_leath/" target="_blank" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="https://github.com/AustinLeath/" target="_blank" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
-						<li><a href="mailto:austinleath18@gmail.com" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
-					</ul>
-					<ul class="copyright">
-						<li>&copy; austinleath.com / r6rc.com - All rights reserved.</li>
-						<li><a href="javascript:history.go(-1)">Go Back</a></li>
-					</ul>
-				</footer>
-		</div>
-		<!-- Scripts -->
-			<script src="/assets/js/jquery.min.js"></script>
-			<script src="/assets/js/jquery.scrolly.min.js"></script>
-			<script src="/assets/js/jquery.dropotron.min.js"></script>
-			<script src="/assets/js/jquery.scrollex.min.js"></script>
-			<script src="/assets/js/skel.min.js"></script>
-			<script src="/assets/js/util.js"></script>
-			<script src="/assets/js/cycleimg.js"></script>
-			<script src="/assets/js/cyclestyle.js"></script>
-			<script src="/assets/js/normalizestyle.js"></script>
-			<script src="/assets/js/time.js"></script>
-			<!--[if lte IE 8]><script src="/assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="/assets/js/main.js"></script>
-	</body>
+   <head>
+      <title>Austin Leath | VRsenal</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content="HTML,CSS,XML,JavaScript,Web,Engineer,Resume,CSS3,HTML5,Austin,Leath,Lethal,Game,Studios,LethalGameStudios">
+      <meta name="description" content="VRsenal is a single player VR game that is both an open world exploration game and physics simulation. Master the landscape as well as a variety of weapons that are laid before you!">
+      <meta name="author" content="Austin Leath">
+      <meta name='copyright' content='Lethal Game Studios'>
+      <meta name='owner' content='Austin Leath'>
+      <link rel="shortcut icon" href="images/favicon.ico" />
+      <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+      <link rel="stylesheet" href="/assets/css/main.css" />
+      <!--[if lte IE 9]>
+      <link rel="stylesheet" href="assets/css/ie9.css" />
+      <![endif]-->
+      <!--[if lte IE 8]>
+      <link rel="stylesheet" href="assets/css/ie8.css" />
+      <![endif]-->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111374271-2"></script>
+      <script>
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+         gtag('config', 'UA-111374271-2');
+      </script>
+   </head>
+   <body id="background">
+      <div id="page-wrapper">
+         <!-- Header -->
+         <header id="header">
+            <h1 id="logo"><a href="">Austin Leath | VRsenal</a></h1>
+            <nav id="nav">
+               <ul>
+                  <li>
+                     <p id="timer"></p>
+                  <li>
+                     <button class="button small btnalign" id="btn1" type="button" onclick="myFunction()">Light Mode</button>
+                  <li>
+                     <button class="button small btnalign" id="btn2" type="button" onclick="myFunction1()" disabled>Dark Mode</button>
+                  <li>
+                     <a href="">Other Information</a>
+                     <ul>
+                        <li><a href="tools.php">Development Tools</a></li>
+                        <li><a href="aboutdeveloper.html">About Me</a></li>
+                        <li><a href="#">VRsenal</a></li>
+                        <li><a href="mmrcalculator.html">R6S | MMR Calculator</a></li>
+                        <li><a href="index.html">Return Home</a></li>
+                     </ul>
+                     <!--DIVIDER-->
+                  </li>
+               </ul>
+               </li>
+               <!-- Add Another Button Here -->
+               </ul>
+            </nav>
+         </header>
+         <!-- Main -->
+         <div id="main" class="wrapper style1">
+            <div class="container">
+               <header class="major">
+                  <h2>Austin Leath | VRsenal</h2>
+                  <section>
+                     <h5>A hub for all things VRsenal</h5>
+                  </section>
+               </header>
+               <!-- Content -->
+               <section id="content">
+                  <img class="image fit" id="image-1" src="images/img1.png" alt="" />
+                  <img class="image fit" id="image-2" src="images/img2.png" alt="" />
+                  <img class="image fit" id="image-3" src="images/img3.png" alt="" />
+                  <img class="image fit" id="image-4" src="images/img4.png" alt="" />
+                  <h3>VRsenal / HTC Vive supported / Independently developed</h3>
+                  <p>VRsenal is a single player HTC Vive supported virtual reality game which is both an exploration game and a physics simulation. Explore the vast lands of VRsenal while interacting with random objects that are scattered throughout the many environments in which you will be playing in. VRsenal provides an assortment of weapons for you to interact with including both melee weapons, 3 hand guns and 4 Assault Rifles.</p>
+                  <p>VRsenal is a single player VR game that is both an open world exploration game and physics simulation. Master the landscape as well as a variety of weapons that are laid before you!</p>
+                  <header class="major">
+                     <h3>Features and Functions</h3>
+                  </header>
+                  <!--Divider-->
+                  <ul>
+                     <strong>Features</strong>
+                     <li>Hand Guns</li>
+                     <li>Assault Rifles</li>
+                     <li>Paintball Guns</li>
+                     <li>Bats</li>
+                     <li>Swords</li>
+                     <li>Knives</li>
+                  </ul>
+                  <ul>
+                     <strong>Environments</strong>
+                     <li>Desert Environments</li>
+                     <li>Arctic Environments</li>
+                     <li>Urban Environments</li>
+                     <li>Wilderness Environments</li>
+                  </ul>
+                  <!--Divider-->
+                  </ul>
+               </section>
+               <header class="major">
+                  <h3>Why I chose Early Access for VRsenal</h3>
+               </header>
+               <p>VRsenal is still in its early stages of development. So I would love to show the community what I have done so far and hear what everyone has to say as far as mechanics go. Things are a bit rusty at the moment but they will shape up soon!</p>
+               <h3>Approximately how long will this game be in Early Access?</h3>
+               <p>Originally I planned for this game to go straight to the market. After realizing that the amount of content was a tad bit low, I decided to go to early access. With the Early Access / initial release slated for July 4th, 2017. However, after starting my Senior year for High School I have realized that my focus needs to be on school.<br>For right now, VRsenal will be in early access for te forseeable future.</p>
+               <h3>How is the full version planned to differ from the Early Access version?</h3>
+               <p>There will be more weapons, game modes and places to explore / shoot up. I am also open to suggestions as far as content and overall game design goes.</p>
+               <h3>What is the current state of the Early Access version?</h3>
+               <p>The current state of the Early Access version is pre alpha. The game certainly isn't finished but with the help of the community this game will be a fun experience for everyone!</p>
+               <h3>Will the game be priced differently during and after Early Access?</h3>
+               <p>I plan to gradually raise the price leading up to the final release. That will only happen when most of the major content that I have planned is in place.</p>
+               <h3>How are you planning on involving the Community in your development process?</h3>
+               <p>I will keep the community involved by closely monitoring the forums and reading every suggestion from our players. We will let our players partake in beta builds so they can let us know what they like and what they dislike. I will also have a VRsenal community Discord where players can always stay in touch with me or my moderators.</p>
+               <header class="major">
+                  <h3>VRsenal Trailer</h3>
+               </header>
+               <iframe class="image fit" width="1280" height="720" src="https://www.youtube.com/embed/mcwT7GV4144" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+         </div>
+         <!-- Footer -->
+         <footer id="footer">
+            <ul class="icons">
+               <li><a href="https://twitter.com/austinzleath" target="_blank" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+               <li><a href="https://www.facebook.com/AustinZacharyLeath" target="_blank" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+               <li><a href="https://www.linkedin.com/in/austin-leath-30a39a106/" target="_blank" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+               <li><a href="https://www.instagram.com/austin_leath/" target="_blank" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+               <li><a href="https://github.com/AustinLeath/" target="_blank" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+               <li><a href="mailto:austinleath18@gmail.com" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+            </ul>
+            <ul class="copyright">
+               <li>&copy; austinleath.com / r6rc.com - All rights reserved.</li>
+               <li><a href="javascript:history.go(-1)">Go Back</a></li>
+            </ul>
+         </footer>
+      </div>
+      <!-- Scripts -->
+      <script src="/assets/js/jquery.min.js"></script>
+      <script src="/assets/js/jquery.scrolly.min.js"></script>
+      <script src="/assets/js/jquery.dropotron.min.js"></script>
+      <script src="/assets/js/jquery.scrollex.min.js"></script>
+      <script src="/assets/js/skel.min.js"></script>
+      <script src="/assets/js/util.js"></script>
+      <script src="/assets/js/cycleimg.js"></script>
+      <script src="/assets/js/cyclestyle.js"></script>
+      <script src="/assets/js/normalizestyle.js"></script>
+      <script src="/assets/js/time.js"></script>
+      <!--[if lte IE 8]><script src="/assets/js/ie/respond.min.js"></script><![endif]-->
+      <script src="/assets/js/main.js"></script>
+   </body>
 </html>
