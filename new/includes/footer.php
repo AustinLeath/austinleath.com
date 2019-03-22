b48f2887fc13b38388aa2b0285fc8a186e6221bf
Austin Leath <austinleath18@gmail.com>
2019-03-22 15:13:40 -0500

    Update footer.php

--- new/includes/footer.php
+++ new/includes/footer.php
@@ -29,7 +29,7 @@ echo '
 <h4>Subscribe</h4>
 <p>Keep yourself updated. Subscribe to my newsletter.</p>
 <div class="subscribe-form">
-<form method="post" action="" novalidate="true">
+<form method="post" action="<?php echo basename(__FILE__); ?>" novalidate="true">
    <input type="email" value="" name="email" class="email" id="mc-email" placeholder="johndoe@gmail.com">
    <input type="submit" name="subscribe">
    <label for="mc-email" class="subscribe-message"></label>
