a454ce08086fadb552fe6ccbfde88d0f8e142a2c
Austin Leath <austinleath18@gmail.com>
2019-03-25 15:21:01 -0500

    Update pagenavigation.php

--- new/includes/pagenavigation.php
+++ new/includes/pagenavigation.php
@@ -1,16 +1,18 @@
-<?php echo
-'<div class="pagenav group">
-  <div class="prev-nav">
-   <a href="#" rel="prev">
-     <span>Previous</span>
-     Tips on Minimalist Design
-   </a>
-  </div>
- <div class="next-nav">
-   <a href="#" rel="next">
-     <span>Next</span>
-     Less Is More
-   </a>
- </div>
-</div>';
+<?php 
+echo'
+<div class="pagenav group">
+   <div class="prev-nav">
+      <a href="#" rel="prev">
+      <span>Previous</span>
+      Tips on Minimalist Design
+      </a>
+   </div>
+   <div class="next-nav">
+      <a href="#" rel="next">
+      <span>Next</span>
+      Less Is More
+      </a>
+   </div>
+</div>
+';
 ?>
