4373ee395d7bce78d431bc22144bed8c871266ca
Austin Leath <austinleath18@gmail.com>
2019-03-25 15:19:03 -0500

    Update head.php

--- new/includes/head.php
+++ new/includes/head.php
@@ -4,16 +4,12 @@ echo '
 <meta name="rating" content="General">
 <meta name="author" content="Austin Leath">
 <meta name="keywords" content="Austin,Leath,Resume" />
-<!-- mobile specific metas -->
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<!-- CSS -->
 <link rel="stylesheet" href="css/base.css">
 <link rel="stylesheet" href="css/vendor.css">
 <link rel="stylesheet" href="css/main.css">
-<!-- scripts -->
 <script src="js/modernizr.js"></script>
 <script src="js/pace.min.js"></script>
-<!-- favicons -->
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <link rel="icon" href="favicon.ico" type="image/x-icon">
 ';
