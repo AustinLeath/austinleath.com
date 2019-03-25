42b581ed17ce8f97bd64f4432fcf1b44a504f054
Austin Leath <austinleath18@gmail.com>
2019-03-25 12:37:36 -0500

    Current version as of 3/25/19

--- new/includes/authorprofile.php
+++ new/includes/authorprofile.php
@@ -4,14 +4,15 @@ echo '
  <img src="images/avatars/user.jpg" alt="">
 
  <div class="about">
-   <h4><a href="#">Austin Leath</a></h4>
+   <h4><a href="/">Austin Leath</a></h4>
 
-   <p>Hey there! Welcome to my blog! I am just getting started putting up content, but I promise more will be posted soon!</p>
+   <p>Hey there! Welcome to my site! I am a Senior in high school working on multiple projects that have to do with both web and software development.</p>
 
    <ul class="author-social">
-     <li><a href="https://www.instagram.com/rileighsayen/" target="_blank">Instagram</a></li>
-     <li><a href="https://twitter.com/rileighsayen" target="_blank">Twitter</a></li>
-     <li><a href="https://www.facebook.com/rileigh.sayen" target="_blank">Facebook</a></li>
+     <li><a href="https://github.com/AustinLeath/" target="_blank">GitHub</a></li>
+     <li><a href="https://www.instagram.com/austin_leath/" target="_blank">Instagram</a></li>
+     <li><a href="https://twitter.com/austinzleath" target="_blank">Twitter</a></li>
+     <li><a href="https://www.facebook.com/AustinZacharyLeath" target="_blank">Facebook</a></li>
    </ul>
  </div>
 </div>
