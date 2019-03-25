4ecc9814b7bc15d3b6d96baa4e852251780f6e24
Austin Leath <austinleath18@gmail.com>
2019-03-25 14:43:09 -0500

    Update authorprofile.php

--- new/includes/authorprofile.php
+++ new/includes/authorprofile.php
@@ -2,12 +2,9 @@
 echo '
 <div class="author-profile">
  <img src="images/avatars/user.jpg" alt="">
-
  <div class="about">
    <h4><a href="/">Austin Leath</a></h4>
-
    <p>Hey there! Welcome to my site! I am a Senior in high school working on multiple projects that have to do with both web and software development.</p>
-
    <ul class="author-social">
      <li><a href="https://github.com/AustinLeath/" target="_blank">GitHub</a></li>
      <li><a href="https://www.instagram.com/austin_leath/" target="_blank">Instagram</a></li>
