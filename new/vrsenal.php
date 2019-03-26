ea66278e97324939ec7c7ae2131c28c3f6fbec99
Austin Leath <austinleath18@gmail.com>
2019-03-26 11:44:06 -0500

    Create vrsenal.php

---
+++ new/vrsenal.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]>
+<html class="no-js oldie ie8" lang="en">
+   <![endif]-->
+   <!--[if IE 9 ]>
+   <html class="no-js oldie ie9" lang="en">
+      <![endif]-->
+      <!--[if (gte IE 9)|!(IE)]><!-->
+      <html class="no-js" lang="en">
+         <!--<![endif]-->
+         <head>
+            <title>VRSENAL - austinleath.com</title>
+            <meta name="description" content="VRsenal is a single player VR game that is both an open world exploration game and physics simulation. Master the landscape as well as a variety of weapons that are laid before you!">
+            <?php include 'includes/head.php'; ?>
+            <?php include 'includes/googleanalytics.php'; ?>
+         </head>
+         <body id="top">
+            <?php include 'includes/header.php'; ?>
+            <!-- content -->
+            <section id="content-wrap" class="blog-single">
+               <div class="row">
+                  <div class="col-twelve">
+                     <article class="format-standard">
+                        <div class="content-media">
+                           <div class="post-thumb">
+                              <img src="images/thumbs/single/single-01.jpg">
+                           </div>
+                        </div>
+
+                        <div class="primary-content">
+                           <h1 class="page-title">Page Title Here</h1>
+                           <ul class="entry-meta">
+                              <li class="date">March, 26th, 2019</li>
+                              <li class="cat"><a href="#">Projects</a></li>
+                           </ul>
+                           <p class="lead">This was/is a project that I started when I first bought the <a href="https://www.vive.com/" target="_blank">HTC Vive</a>. This project was supposed to be kind of like <a href="http://h3vr.com/" target="_blank">Hot Dogs, Horseshoes and Hand Grenades</a>, but with a unique twist. The name of the product that I was developing was called VRsenal and was actually accepted to Steam!</p>
+                           
+                           
+                           <p>This is a paragraph, fill me out or delete me.</p>
+                           <p class="tags">
+                              <span>Tagged in :</span>
+                              <a href="#">Uncategorized</a><a href="#">New Beginnings</a>
+                           </p>
+
+                           <?php include 'includes/authorprofile.php'; ?>
+                        </div>
+                        <?php include 'includes/pagenavigation.php'; ?>
+                     </article>
+                  </div>
+               </div>
+            </section>
+            <?php include 'includes/footer.php'; ?>
+            <div id="preloader">
+               <div id="loader"></div>
+            </div>
+            <!-- Java Script -->
+            <script src="js/jquery-2.1.3.min.js"></script>
+            <script src="js/plugins.js"></script>
+            <script src="js/main.js"></script>
+         </body>
+      </html>
