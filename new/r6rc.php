65ed7b0eec03b73ec6b08c13d9b03ad9978b724c
Austin Leath <austinleath18@gmail.com>
2019-03-26 15:55:26 -0500

    Create r6rc.php

---
+++ new/r6rc.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<!--[if IE 8 ]>
+<html class="no-js oldie ie8" lang="en">
+   <![endif]-->
+   <!--[if IE 9 ]>
+   <html class="no-js oldie ie9" lang="en">
+      <![endif]-->
+      <!--[if (gte IE 9)|!(IE)]><!-->
+      <html class="no-js" lang="en">
+         <!--<![endif]-->
+         <head>
+            <title>R6RC - austinleath.com</title>
+            <meta name="description" content="">
+            <?php include 'includes/head.php'; ?>
+            <?php include 'includes/googleanalytics.php'; ?>
+         </head>
+         <body id="top">
+            <?php include 'includes/header.php'; ?>
+            <!-- content -->
+            <section id="content-wrap" class="blog-single">
+               <div class="row">
+                  <div class="col-twelve">
+                     <article class="format-standard">
+                        <div class="content-media">
+                           <div class="post-thumb">
+                              <img src="images/thumbs/single/single-01.jpg">
+                           </div>
+                        </div>
+
+                        <div class="primary-content">
+                           <h1 class="page-title">R6RC</h1>
+                           <ul class="entry-meta">
+                              <li class="date">March, 26th, 2019</li>
+                              <li class="cat"><a href="projects.php">Projects</a></li>
+                           </ul>
+                           <p class="lead"></p>
+
+                           <p class="tags">
+                              <span>Tagged in :</span>
+                              <a href="projects.php">Projects</a>
+                           </p>
+                           <?php include 'includes/authorprofile.php'; ?>
+                        </div>
+                        <?php include 'includes/pagenavigation.php'; ?>
+                     </article>
+                  </div>
+               </div>
+            </section>
+            <?php include 'includes/footer.php'; ?>
+            <div id="preloader">
+               <div id="loader"></div>
+            </div>
+            <!-- JavaScript -->
+            <script src="js/jquery-2.1.3.min.js"></script>
+            <script src="js/plugins.js"></script>
+            <script src="js/main.js"></script>
+         </body>
+      </html>
