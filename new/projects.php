22fbf15159597b0521ae78dc9304bca29ecf42d8
Austin Leath <austinleath18@gmail.com>
2019-03-26 12:40:27 -0500

    Create projects.php

---
+++ new/projects.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]>
+<html class="no-js oldie ie8" lang="en">
+   <![endif]-->
+   <!--[if IE 9 ]>
+   <html class="no-js oldie ie9" lang="en">
+      <![endif]-->
+      <!--[if (gte IE 9)|!(IE)]><!-->
+      <html class="no-js" lang="en">
+         <!--<![endif]-->
+         <head>
+            <title>Projects - austinleath.com</title>
+            <meta name="description" content="Here is where you can find an organized list of all of my projects">
+            <?php include 'includes/head.php'; ?>
+	          <?php include 'includes/googleanalytics.php'; ?>
+         </head>
+         <body id="top">
+	       <?php include 'includes/header.php'; ?>
+            <section id="page-header">
+               <div class="row current-cat">
+                  <div class="col-full">
+                     <h1>Category: Projects</h1>
+                  </div>
+               </div>
+            </section>
+            <section id="bricks" class="with-top-sep">
+               <div class="row masonry">
+                  <!-- brick-wrapper -->
+                  <div class="bricks-wrapper">
+                     <div class="grid-sizer"></div>
+                     <article class="brick entry format-standard animate-this">
+                        <div class="entry-thumb">
+                           <a href="single-standard.html" class="thumb-link">
+                           <img src="images/thumbs/diagonal-building.jpg" alt="building">
+                           </a>
+                        </div>
+                        <div class="entry-text">
+                           <div class="entry-header">
+                              <div class="entry-meta">
+                                 <span class="cat-links">
+                                 <a href="#">Projects</a>
+                                 </span>
+                              </div>
+                              <h1 class="entry-title"><a href="single-standard.html">VRSENAL</a></h1>
+                           </div>
+                           <div class="entry-excerpt">
+                             This article contains information about one of my projects named VRsenal.
+                           </div>
+                        </div>
+                     </article>
+
+                  </div>
+                  <!-- end brick-wrapper -->
+               </div>
+               <!-- end row -->
+               <div class="row">
+                  <nav class="pagination">
+                     <span class="page-numbers prev inactive">Prev</span>
+                     <span class="page-numbers current">1</span>
+                     <a href="#" class="page-numbers">2</a>
+                     <a href="#" class="page-numbers">3</a>
+                     <a href="#" class="page-numbers">4</a>
+                     <a href="#" class="page-numbers">5</a>
+                     <a href="#" class="page-numbers">6</a>
+                     <a href="#" class="page-numbers">7</a>
+                     <a href="#" class="page-numbers">8</a>
+                     <a href="#" class="page-numbers">9</a>
+                     <a href="#" class="page-numbers next">Next</a>
+                  </nav>
+               </div>
+            </section>
+            <!-- bricks -->
+	          <?php include 'includes/footer.php'; ?>
+            <div id="preloader">
+               <div id="loader"></div>
+            </div>
+            <!-- JavaScript -->
+            <script src="js/jquery-2.1.3.min.js"></script>
+            <script src="js/plugins.js"></script>
+            <script src="js/main.js"></script>
+         </body>
+      </html>
