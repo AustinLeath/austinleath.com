ef11053fbdcabb1343ecc19473b8147e8b643ca4
Austin Leath <austinleath18@gmail.com>
2019-03-29 11:40:29 -0500

    Update about.php

--- new/about.php
+++ new/about.php
@@ -7,15 +7,15 @@
       <![endif]-->
       <!--[if (gte IE 9)|!(IE)]><!-->
       <html class="no-js" lang="en">
-         <!--<![endif]-->
-         <head>
-            <title>About Me - austinleath.com</title>
-            <meta name="description" content="Welcome to my about me page!">
-	    <?php include 'includes/head.php'; ?>
-	    <?php include 'includes/googleanalytics.php'; ?>
-         </head>
+      <!--<![endif]-->
+      <head>
+         <title>About Me - austinleath.com</title>
+         <meta name="description" content="Welcome to my about me page!">
+	       <?php include 'includes/head.php'; ?>
+	       <?php include 'includes/googleanalytics.php'; ?>
+      </head>
          <body id="top">
-	 <?php include 'includes/header.php'; ?>
+	          <?php include 'includes/header.php'; ?>
             <!-- content -->
             <section id="content-wrap" class="site-page">
                <div class="row">
@@ -25,39 +25,45 @@
                            <img src="images/thumbs/about-us.jpg">
                         </div>
                         <div class="primary-content">
-                           <h1 class="entry-title add-bottom">Learn More About Us.</h1>
-                           <p class="lead">Lorem ipsum Nisi cillum reprehenderit minim sunt dolore dolor eiusmod eu aliquip ad ut sint dolore laborum voluptate ullamco dolore aliquip enim. Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing</p>
-                           <p>Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa occaecat ut laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat in adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.</p>
-                           <p>Lorem ipsum Cillum sit sunt dolore non aute enim pariatur deserunt magna reprehenderit veniam officia ullamco eiusmod laborum commodo veniam elit proident enim sit cillum ex aliquip dolore labore sint ut deserunt officia veniam consectetur in in quis eu consectetur non sint Duis mollit Ut magna irure.</p>
-                           <div class="row block-1-2 block-tab-full">
-                              <div class="bgrid">
-                                 <h3>Who.</h3>
-                                 <p>Lorem ipsum Nisi amet fugiat eiusmod et aliqua ad qui ut nisi Ut aute anim mollit fugiat qui sit ex occaecat et eu mollit nisi pariatur fugiat deserunt dolor veniam reprehenderit aliquip magna nisi consequat aliqua veniam in aute ullamco Duis laborum ad non pariatur sit.</p>
-                              </div>
-                              <div class="bgrid">
-                                 <h3>When.</h3>
-                                 <p>Lorem ipsum Nisi amet fugiat eiusmod et aliqua ad qui ut nisi Ut aute anim mollit fugiat qui sit ex occaecat et eu mollit nisi pariatur fugiat deserunt dolor veniam reprehenderit aliquip magna nisi consequat aliqua veniam in aute ullamco Duis laborum ad non pariatur sit.</p>
-                              </div>
-                              <div class="bgrid">
-                                 <h3>What.</h3>
-                                 <p>Lorem ipsum Nisi amet fugiat eiusmod et aliqua ad qui ut nisi Ut aute anim mollit fugiat qui sit ex occaecat et eu mollit nisi pariatur fugiat deserunt dolor veniam reprehenderit aliquip magna nisi consequat aliqua veniam in aute ullamco Duis laborum ad non pariatur sit.</p>
-                              </div>
-                              <div class="bgrid">
-                                 <h3>How.</h3>
-                                 <p>Lorem ipsum Nisi amet fugiat eiusmod et aliqua ad qui ut nisi Ut aute anim mollit fugiat qui sit ex occaecat et eu mollit nisi pariatur fugiat deserunt dolor veniam reprehenderit aliquip magna nisi consequat aliqua veniam in aute ullamco Duis laborum ad non pariatur sit.</p>
-                              </div>
-                           </div>
+                           <h1 class="entry-title add-bottom">About Me</h1>
+                           <p class="lead">Welcome to my about me page. This is where you can learn about me and what I do.</p>
+                           <p>I am a student developer looking for summer internships in the web development industry! I am a highly motivated self starter who is constantly learning new industry techniques.</p>
+
+                           <p class="lead">I will be attending the University of North Texas, <a href="https://en.wikipedia.org/wiki/Denton,_Texas" target="_blank">Denton, TX</a> and will be studying Information Technology and Computer Science.</p>
+
+                         <h3>Skills</h3>
+                           <ul>
+                             <strong>General Knowledge</strong>
+                             <li>Computer System Building - 6 years</li>
+                             <li>GIT / Version Control - 4 years</li>
+                             <li>SSH - 4 years</li>
+                             <li>Node.js - 1 year</li>
+                             <li>Java - 1 year</li>
+                           </ul>
+                           <ul>
+                             <strong>Web Languages</strong>
+                             <li>HTML / HTML5 - 5 years</li>
+                             <li>CSS - 5 years</li>
+                             <li>JavaScript - 3 years</li>
+                             <li>PHP - 1 year</li>
+                           </ul>
+                           <h3>My Methods & Tools I Use</h3>
+                           <ol>
+                             <li><a href="https://semver.org/" target="_blank">Semantic Software Versioning</a>: I use Semantic Software Versioning when I publish my releases. It helps me organize my thoughts (and files) as I move along with my work.</li>
+                             <li><a href="https://atom.io" target="_blank">Atom Text Editor</a>: I always tend to use the Atom text editor for anything and everything code related.</li>
+                             <li><a href="https://github.com/">GitHub</a>: Ever since I have learned of version control, I have used GitHub. Github is an amazing tool that has helped me compile and organize all of my projects.</li>
+                           </ol>
                         </div>
                      </section>
                   </div>
                </div>
             </section>
             <!-- end content -->
-	    <?php include 'includes/footer.php'; ?>
+	          <?php include 'includes/footer.php'; ?>
             <div id="preloader">
                <div id="loader"></div>
             </div>
-            <!-- Java Script -->
+            <!-- JavaScript -->
             <script src="js/jquery-2.1.3.min.js"></script>
             <script src="js/plugins.js"></script>
             <script src="js/main.js"></script>
