dbf1d4b42e485180d52215c5324a33da316f1977
Austin Leath <austinleath18@gmail.com>
2019-04-01 14:41:34 -0500

    Update createpost.php

--- new/includes/createpost.php
+++ new/includes/createpost.php
@@ -11,24 +11,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$password = mysqli_real_escape_string($conn,$_POST["password"]);
-$filename = mysqli_real_escape_string($conn,$_POST["filename"]);
+$password = htmlspecialchars(mysqli_real_escape_string($conn,$_POST["password"]));
+$filename = htmlspecialchars(mysqli_real_escape_string($conn,$_POST["filename"]));
 
 if(empty($password)) {
   echo "";
-  $conn->close();
 } else if($password != "sadiepiper1") {
   echo '<div class="alert-box ss-error hideit"> <p>The password you entered was incorrect. Please try again.</p> <i class="fa fa-times close"></i> </div>';
-  $conn->close();
 } else if(empty($filename)) {
   echo '<div class="alert-box ss-error hideit"> <p>You must fill complete the entire form to create a post.</p> <i class="fa fa-times close"></i> </div>';
-  $conn->close();
 } else if(file_exists("$filename" . ".php")) {
   echo '<div class="alert-box ss-error hideit"> <p>The file you are trying to create already exists.</p> <i class="fa fa-times close"></i> </div>';
-  $conn->close();
 } else {
   copy('template.php',"$filename" . ".php");
   echo '<div class="alert-box ss-success hideit"> <p>New file created successfully. The file that was created was:  '. $filename . '.php' . ' and is located <a href="/'. $filename .'.php">here</a></p> <i class="fa fa-times close"></i> </div>';
-  $conn->close();
 }
+$conn->close();
 ?>
