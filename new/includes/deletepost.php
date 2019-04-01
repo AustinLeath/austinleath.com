df8b6f7926f4186a8e5c4aa788e334a42924be71
Austin Leath <austinleath18@gmail.com>
2019-04-01 14:33:06 -0500

    Update deletepost.php

--- new/includes/deletepost.php
+++ new/includes/deletepost.php
@@ -16,19 +16,15 @@ $filename = htmlspecialchars(mysqli_real_escape_string($conn,$_POST["filename"])
 
 if(empty($password)) {
   echo "";
-  $conn->close();
 } else if($password != "test") {
   echo '<div class="alert-box ss-error hideit"> <p>The password you entered was incorrect. Please try again.</p> <i class="fa fa-times close"></i> </div>';
-  $conn->close();
 } else if(empty($filename)) {
   echo '<div class="alert-box ss-error hideit"> <p>You must fill complete the entire form to delete a post.</p> <i class="fa fa-times close"></i> </div>';
-  $conn->close();
 } else if(!file_exists("$filename" . ".php") or $filename == "deletepost" or $filename == "createpost" or $filename == "contact" or $filename == "about" or $filename == "delete" or $filename == "unsubscribe") {
   echo '<div class="alert-box ss-error hideit"> <p>The file you are trying to delete does not exist or cannot be deleted.</p> <i class="fa fa-times close"></i> </div>';
-  $conn->close();
 } else {
   unlink("$filename" . ".php");
   echo '<div class="alert-box ss-success hideit"> <p>File deleted successfully.</p> <i class="fa fa-times close"></i> </div>';
-  $conn->close();
 }
+$conn->close();
 ?>
