6a2d75d56ba62ccad4929b04e160f3b4c3ecdd82
Austin Leath <austinleath18@gmail.com>
2019-04-02 13:27:55 -0500

    Create r6rcbot.php

---
+++ new/r6rcbot.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<!--[if IE 8 ]>
+<html class="no-js oldie ie8" lang="en">
+   <![endif]-->
+   <!--[if IE 9 ]>
+   <html class="no-js oldie ie9" lang="en">
+      <![endif]-->
+      <!--[if (gte IE 9)|!(IE)]><!-->
+      <html class="no-js" lang="en">
+         <!--<![endif]-->
+         <head>
+            <title>R6RCBOT - austinleath.com</title>
+            <meta name="description" content="I talk about the Discord bot I made as a project to get me introduced into Python">
+            <?php include 'includes/head.php'; ?>
+            <?php include 'includes/googleanalytics.php'; ?>
+         </head>
+         <body id="top">
+            <?php include 'includes/header.php'; ?>
+            <!-- content -->
+            <section id="content-wrap" class="blog-single">
+               <div class="row">
+                  <div class="col-twelve">
+                     <article class="format-standard">
+                        <div class="content-media">
+                           <div class="post-thumb">
+                              <img src="images/r6rcimg.png">
+                           </div>
+                        </div>
+
+                        <div class="primary-content">
+                           <h1 class="page-title">R6RCBOT</h1>
+                           <ul class="entry-meta">
+                              <li class="date">April, 2nd, 2019</li>
+                              <li class="cat"><a href="projects.php">Projects</a></li>
+                           </ul>
+                       <!-- DATA HERE -->
+
+                           <p class="tags">
+                              <span>Tagged in :</span>
+                              <a href="projects.php">Projects</a>
+                           </p>
+                           <?php include 'includes/authorprofile.php'; ?>
+                        </div>
+                        <?php include 'includes/pagenavigation.php'; ?>
+                     </article>
+                  </div>
+               </div>
+            </section>
+            <?php include 'includes/footer.php'; ?>
+            <div id="preloader">
+               <div id="loader"></div>
+            </div>
+            <!-- JavaScript -->
+            <script src="js/jquery-2.1.3.min.js"></script>
+            <script src="js/plugins.js"></script>
+            <script src="js/main.js"></script>
+         </body>
+      </html>
