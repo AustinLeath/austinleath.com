ebba13169712d24810ac44d4de52ab992744ef26
AustinLeath <austinleath18@gmail.com>
2019-05-10 01:13:03 -0500

    commit to fix commit

--- finqr/hpass.php
+++ finqr/hpass.php
@@ -3,16 +3,10 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin');
 
 $jsonString = file_get_contents("php://input");
-/*
-$phpObject = json_decode($jsonString);
-$newJsonString = json_encode($phpObject);
-header('Content-Type: application/json');
-echo $newJsonString;
-*/
+
 $my_file = 'file.json';
 $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
 $data = $jsonString;
 fwrite($handle, $data);
 fclose($handle);
-
 ?>
