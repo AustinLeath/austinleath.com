b7dec7082ef6323d80206d642ccb78193509ee14
AustinLeath <austinleath18@gmail.com>
2019-05-17 02:02:52 -0500

    Current version as of 5/17/19

--- finqr/hpass.php
+++ finqr/hpass.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Origin');
 $finputData = file_get_contents("php://input");
 $user = json_decode($finputData);
 
-$filename = str_replace(' ','',$user->name).'.json';
+$filename = strtolower(str_replace(' ','',$user->name)).'.json';
 $handle = fopen("users/".$filename, 'w') or die('Cannot open file: '.$filename);
 fwrite($handle, $finputData);
 fclose($handle);
