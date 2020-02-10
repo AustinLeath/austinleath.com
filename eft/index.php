5bfca890319830b47f434e611fb5e70bbc7f6195
AustinLeath <austinleath18@gmail.com>
2020-02-09 19:04:33 -0600

    Create index.php

---
+++ eft/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Tarkov Map Directory</title>
+  </head>
+  <body>
+    <ul>
+      <li>
+        <a href="https://austinleath.com/eft/customs">https://austinleath.com/eft/customs</a>
+        <ul>
+          <li><a href="https://austinleath.com/eft/dorms">https://austinleath.com/eft/dorms</a></li>
+        </ul>
+      </li>
+      <li><a href="https://austinleath.com/eft/customs">https://austinleath.com/eft/woods</a></li>
+      <li><a href="https://austinleath.com/eft/customs">https://austinleath.com/eft/interchange</a></li>
+      <li><a href="https://austinleath.com/eft/customs">https://austinleath.com/eft/reserve</a></li>
+    </ul>
+  </body>
+</html>
